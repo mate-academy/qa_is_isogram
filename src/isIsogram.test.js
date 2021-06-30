e91d3727650961aab21d62e82e9a773c49a2aa6c
anna <anna.sydorenko.m@gmail.com>
2021-06-30 17:26:11 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -8,12 +8,30 @@ describe(`Function 'isIsogram':`, () => {
   });
 
   it(`should return a boolean`, () => {
-
+    expect(typeof isIsogram('')).toBe('boolean');
   });
 
-  it(`should be case sensitive`, () => {
-
+  it(`should not be case sensitive`, () => {
+    expect(isIsogram('Alarm')).toBe(false);
   });
 
-  // write more tests here
+  it(`should return 'true' if the string is empty`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`must return 'true' if the characters of value are unique`, () => {
+    expect(isIsogram('Desktop')).toBe(true);
+  });
+
+  it(`must return 'false' if the characters of value are not unique`, () => {
+    expect(isIsogram('deep')).toBe(false);
+  });
+
+  it(`must return 'false' if the value dublicate by a space`, () => {
+    expect(isIsogram('Desktop Desktop')).toBe(false);
+  });
+
+  it(`must return 'true' if the is only one characters of value`, () => {
+    expect(isIsogram('g')).toBe(true);
+  });
 });
