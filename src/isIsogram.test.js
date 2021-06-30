9ddd4b0d8e1381b4f57a347a2b31560c955664b0
Alkayde <morozovnikita5899@gmail.com>
2021-06-30 13:54:01 +0300

    Test

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -8,12 +8,33 @@ describe(`Function 'isIsogram':`, () => {
   });
 
   it(`should return a boolean`, () => {
-
+    expect(typeof isIsogram('word')).toBe('boolean');
   });
 
-  it(`should be case sensitive`, () => {
-
+  it(`should not be case sensitive`, () => {
+    expect(isIsogram('WORD')).toBe(true);
   });
 
-  // write more tests here
+  it(`should return 'true' if string is blank`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`should return 'false' if string has duplicated consecutive symbols`,
+    () => {
+      expect(isIsogram('School')).toBe(false);
+    });
+
+  it(`should return 'false' if string has duplicated inconsecutive symbols`,
+    () => {
+      expect(isIsogram('Nikita')).toBe(false);
+    });
+
+  it(`should treat Cyrillic symbols as valid`, () => {
+    expect(isIsogram('Обед')).toBe(true);
+  });
+
+  it(`should compare similar by writing cyrillic and latin symbols 
+  as duplicates`, () => {
+    expect(isIsogram('Аннa')).toBe(false);
+  });
 });
