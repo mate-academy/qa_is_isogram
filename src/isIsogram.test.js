9cc5d36dcd7f7505bb6b3b3546cfaf65edb1d8a6
astakh21 <astakhov.mark@gmail.com>
2021-07-01 09:08:55 -0400

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -8,12 +8,28 @@ describe(`Function 'isIsogram':`, () => {
   });
 
   it(`should return a boolean`, () => {
-
+    expect(typeof isIsogram('jp')).toBe('boolean');
   });
 
-  it(`should be case sensitive`, () => {
-
+  it(`should not be case sensitive`, () => {
+    expect(isIsogram('nIkita')).toBe(false);
   });
 
-  // write more tests here
+  it(`should return 'true' if string is blank`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`should return 'false' if string is not isogram 
+  and the repeating letters are arranged in a row`, () => {
+    expect(isIsogram('truue')).toBe(false);
+  });
+
+  it(`should return 'false' if string is not isogram 
+  and the repeating letters are not arranged in a row`, () => {
+    expect(isIsogram('trueu')).toBe(false);
+  });
+
+  it(`should return 'true' if string is isogram`, () => {
+    expect(isIsogram('letqwry')).toBeTruthy();
+  });
 });
