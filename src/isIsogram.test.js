6d1b9baa52d4b6ed54219167f7d4a2147eda6411
MariiaSpiridonova <mariia.spiridonova@gmail.com>
2021-07-01 16:05:42 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -8,12 +8,24 @@ describe(`Function 'isIsogram':`, () => {
   });
 
   it(`should return a boolean`, () => {
-
+    expect(typeof isIsogram('copyright')).toBe('boolean');
   });
 
   it(`should be case sensitive`, () => {
+    const result = isIsogram('loOk');
 
+    expect(result).toBeFalsy();
   });
 
-  // write more tests here
+  it(`should return 'true' if the input is empty`, () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
+
+  it(`should return 'true' if the word is an isogram`, () => {
+    expect(isIsogram('playground')).toBeTruthy();
+  });
+
+  it(`should return 'false' if the word is not an isogram`, () => {
+    expect(isIsogram('abracadabra')).toBeFalsy();
+  });
 });
