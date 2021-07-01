23abff60dffd1a47f4ee78aaac09a4fcce8ebe62
annasisoian <anna.sisoian@gmail.com>
2021-07-02 00:22:06 +0300

    tested function

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -8,12 +8,26 @@ describe(`Function 'isIsogram':`, () => {
   });
 
   it(`should return a boolean`, () => {
-
+    expect(typeof isIsogram('word')).toBe('boolean');
   });
 
-  it(`should be case sensitive`, () => {
-
+  it(`should not be case sensitive`, () => {
+    expect(isIsogram('WoRd')).toBeTruthy();
   });
 
-  // write more tests here
+  it(`should return 'true' if the string is empty`, () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
+
+  it(`should work with both letters and numbers`, () => {
+    expect(isIsogram('KeY453534')).toBeFalsy();
+  });
+
+  it(`should return 'true' if 'Omit' is used`, () => {
+    expect(isIsogram('Omit')).toBe(true);
+  });
+
+  it(`should return 'false' if 'deterrent' is used`, () => {
+    expect(isIsogram('deterrent')).toBe(false);
+  });
 });
