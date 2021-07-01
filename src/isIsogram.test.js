74cb1d1eae69b9120d1f4ad64b86ed6848556711
Kalganova <nikavik1990@gmail.com>
2021-07-01 16:41:41 +0400

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -8,12 +8,25 @@ describe(`Function 'isIsogram':`, () => {
   });
 
   it(`should return a boolean`, () => {
+    expect(typeof isIsogram('abc')).toBe('boolean');
 
   });
 
   it(`should be case sensitive`, () => {
+    expect(isIsogram('Adam')).toBe(false);
 
   });
 
-  // write more tests here
+  it(`should return true if word contains one letter`, () => {
+    expect(isIsogram('J')).toBe(true);
+  });
+
+  it(`should return false if word contains repeated letters`, () => {
+    expect(isIsogram('Apple')).toBe(false);
+  });
+
+  it(`should return true if word contains unique letters`, () => {
+    expect(isIsogram('Crazy')).toBe(true);
+  });
+  
 });
