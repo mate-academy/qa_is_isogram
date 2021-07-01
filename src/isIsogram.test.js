d5cd9162768f60a2ae53eedb7d6dd9c603aebfa5
Aalderaan <chyregd@gmail.com>
2021-07-01 14:23:33 +0200

    Solution to is_isogram

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -8,12 +8,30 @@ describe(`Function 'isIsogram':`, () => {
   });
 
   it(`should return a boolean`, () => {
+    const checkBoolean = isIsogram('');
 
+    expect(typeof checkBoolean === 'boolean').toBeTruthy();
   });
 
-  it(`should be case sensitive`, () => {
-
+  it(`should not be case sensitive`, () => {
+    expect(isIsogram('MmNn')).toBeFalsy();
   });
 
-  // write more tests here
+  it(`should return 'true' if word = '123'`, () => {
+    expect(isIsogram('123')).toBeTruthy();
+  });
+
+  it(`should return 'false' if numbers are not 
+  unique: word = '333'`, () => {
+    expect(isIsogram('333')).toBeFalsy();
+  });
+
+  it(`should return 'true' if word = ''`, () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
+
+  it(`should return 'false' if letters 
+  are no unique: word = 'look'`, () => {
+    expect(isIsogram('look')).toBeFalsy();
+  });
 });
