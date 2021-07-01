767067a3bb3a34440aa16d2bc3b2e8155d9643a5
VasylLozhnikov <Lozhnikovve@gmail.com>
2021-07-01 16:20:25 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -8,12 +8,29 @@ describe(`Function 'isIsogram':`, () => {
   });
 
   it(`should return a boolean`, () => {
-
+    expect(typeof isIsogram('x'))
+      .toBe('boolean');
   });
 
-  it(`should be case sensitive`, () => {
-
+  it(`should not be case sensitive`, () => {
+    expect(isIsogram(`AaBbCc`))
+      .toBe(false);
   });
 
-  // write more tests here
+  it(`should return 'true' value if input is ' '`, () => {
+    expect(isIsogram(' '))
+      .toBe(true);
+  });
+
+  it(`should return 'false' if the same characters aren't 
+  next to each other`, () => {
+    expect(isIsogram('vasiliy'))
+      .toBe(false);
+  });
+
+  it(`should return 'false' if the same characters are 
+  next to each other`, () => {
+    expect(isIsogram('vassiliy'))
+      .toBe(false);
+  });
 });
