b60e5885db757a9768ae9cc28d30f394f62873b1
ostapdev <ostap.baraniak@yahoo.com>
2021-07-01 13:16:36 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -8,12 +8,27 @@ describe(`Function 'isIsogram':`, () => {
   });
 
   it(`should return a boolean`, () => {
-
+    expect(typeof isIsogram('')).toBe('boolean');
   });
 
-  it(`should be case sensitive`, () => {
-
+  it(`should not be case sensitive`, () => {
+    expect(isIsogram('ATOM')).toBe(true);
   });
 
+  it(`should return 'true' if the string is empty`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`should return 'false' if the characters of value are not unique`, () => {
+    expect(isIsogram('call')).toBe(false);
+  });
+
+  it(`should return 'false' if the value dublicates by a space`, () => {
+    expect(isIsogram('atom atom')).toBe(false);
+  });
+
+  it(`should return 'true' if the is only one characters of value`, () => {
+    expect(isIsogram('s')).toBe(true);
+  });
   // write more tests here
 });
