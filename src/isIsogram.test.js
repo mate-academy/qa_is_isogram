ebfca4b34cf897a6dd67fb08056cfef5429c3973
Pavlo Starynskyi <starinskiy@gmail.com>
2021-08-05 13:30:17 +0300

    first commit

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -8,12 +8,30 @@ describe(`Function 'isIsogram':`, () => {
   });
 
   it(`should return a boolean`, () => {
-
+    expect(typeof isIsogram('')).toBe('boolean');
   });
 
   it(`should be case sensitive`, () => {
-
+    expect(isIsogram('RhCp')).toBe(true);
   });
 
-  // write more tests here
+  it(`should return 'true' for empty string`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`should return 'false' if there are 2 same letters`, () => {
+    expect(isIsogram('aqua')).toBe(false);
+  });
+
+  it(`should return 'true' if string consists of one letter`, () => {
+    expect(isIsogram('S')).toBe(true);
+  });
+
+  it(`should return 'true' if string includes special symbol`, () => {
+    expect(isIsogram('MoNey$')).toBe(true);
+  });
+
+  it(`should return 'true' if input includes space`, () => {
+    expect(isIsogram('My LoVe')).toBe(true);
+  });
 });
