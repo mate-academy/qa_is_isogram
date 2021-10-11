08aafef950f23c150a516ae4346da787c1a7c18b
maksymdonets <done4ik4@gmail.com>
2021-10-11 15:13:38 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -8,12 +8,33 @@ describe(`Function 'isIsogram':`, () => {
   });
 
   it(`should return a boolean`, () => {
-
+    expect(typeof isIsogram).toBeTruthy();
   });
 
   it(`should be case sensitive`, () => {
-
+    expect(isIsogram('PlayGround')).toBeTruthy();
   });
 
-  // write more tests here
+  it(`should return false with similar letters`, () => {
+    expect(isIsogram('look')).toBeFalsy();
+  });
+
+  it(`should return false with similar letters in differnt
+  cases`, () => {
+    expect(isIsogram('LookAdam')).toBeFalsy();
+  });
+
+  it(`should return true with different letters`, () => {
+    expect(isIsogram('abcdefj')).toBeTruthy();
+  });
+
+  it(`should return true empty value`, () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
 });
+// Examples:
+
+// isIsogram('playgrounds') === true
+// isIsogram('look') === false
+// isIsogram('Adam') === false
+// isIsogram('') === true
