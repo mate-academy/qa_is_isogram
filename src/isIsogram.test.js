f7929b5e6a846cb193773457c16b8f225159bb81
mykolaPakhniuk <pakhniukm@gmail.com>
2021-10-11 18:54:38 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -8,11 +8,31 @@ describe(`Function 'isIsogram':`, () => {
   });
 
   it(`should return a boolean`, () => {
-
+    expect(isIsogram).toBeTruthy();
   });
 
   it(`should be case sensitive`, () => {
+    expect(isIsogram('Kyiv')).toBeTruthy();
+  });
 
+  it(`should return a boolean`, () => {
+    expect(isIsogram('qwertyui')).toBeTruthy();
+  });
+
+  it(`should return a falsy`, () => {
+    expect(isIsogram('qqwerty')).toBeFalsy();
+  });
+
+  it(`should return a falsy`, () => {
+    expect(isIsogram('Qqwerty')).toBeFalsy();
+  });
+
+  it(`should return a true`, () => {
+    expect(isIsogram(' ')).toBeTruthy();
+  });
+
+  it(`should return a true`, () => {
+    expect(isIsogram('213')).toBeTruthy();
   });
 
   // write more tests here
