071c83f1ebc0b1d7489c7f59dbf9c25666de5a32
mykolaPakhniuk <pakhniukm@gmail.com>
2021-10-12 17:06:50 +0300

    Fixes after code review

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -15,23 +15,25 @@ describe(`Function 'isIsogram':`, () => {
     expect(isIsogram('Kyiv')).toBeTruthy();
   });
 
-  it(`should return a boolean`, () => {
+  it(`should return a boolean if there are no identical 
+  letters in the word`, () => {
     expect(isIsogram('qwertyui')).toBeTruthy();
   });
 
-  it(`should return a falsy`, () => {
+  it(`should return a falsy if there are identical letters in the word`, () => {
     expect(isIsogram('qqwerty')).toBeFalsy();
   });
 
-  it(`should return a falsy`, () => {
+  it(`should return a falsy if there are one capital letter the same 
+  as one small letter`, () => {
     expect(isIsogram('Qqwerty')).toBeFalsy();
   });
 
-  it(`should return a true`, () => {
+  it(`should return a true if isogram are empty`, () => {
     expect(isIsogram(' ')).toBeTruthy();
   });
 
-  it(`should return a true`, () => {
+  it(`should return a true if isogram no contain the same number`, () => {
     expect(isIsogram('213')).toBeTruthy();
   });
 
