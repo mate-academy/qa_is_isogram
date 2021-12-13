dd196a238708e516bb32db84ca53c65585f14fe1
Iren Gorak <iren.horak@gmail.com>
2021-12-13 17:05:23 +0200

    Solution 4

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -1,7 +1,29 @@
+'use strict';
+
 describe('isIsogram', () => {
   const { isIsogram } = require('./isIsogram');
 
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
+  });
+
+  it('should be true when empty line', () => {
+    expect(isIsogram('')).toEqual(true);
+  });
+
+  it('should be true when letters are not the same', () => {
+    expect(isIsogram('playgrounds')).toEqual(true);
+  });
+
+  it('should be false  when letters the same', () => {
+    expect(isIsogram('look')).toEqual(false);
+  });
+
+  it('should be true when letters the same', () => {
+    expect(isIsogram('Adam')).toEqual(false);
+  });
+
+  it('should return true, when letters case-insensitive', () => {
+    expect(isIsogram('Faus')).toEqual(true);
   });
 });
