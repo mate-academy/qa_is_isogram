b87b94468d88b3480d100b23b8701f805981786a
Katy Mozharovska <katy.mozharovska@gmail.com>
2021-12-22 18:39:10 +0200

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -1,7 +1,26 @@
+'use strict';
+
 describe('isIsogram', () => {
   const { isIsogram } = require('./isIsogram');
 
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
+  });
+
+  it(`should return true for empty string`, () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
+
+  it(`should return true for isogram as word "isogram"`, () => {
+    expect(isIsogram('isogram')).toBeTruthy();
+  });
+
+  it(`should return false for non-isogram word as "every"`, () => {
+    expect(isIsogram('every')).toBeFalsy();
+  });
+
+  it(`should return false for capitalized non-isogram word
+  as "Alabama"`, () => {
+    expect(isIsogram('Alabama')).toBeFalsy();
   });
 });
