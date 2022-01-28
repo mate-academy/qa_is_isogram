460971d90919b70fba26a8e96afc180a284e80cc
Andrii <badekha@gmail.com>
2022-01-28 20:56:47 +0200

    solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -4,6 +4,37 @@ describe('isIsogram', () => {
   const { isIsogram } = require('./isIsogram');
 
   it(`should be declared`, () => {
-    expect(isIsogram).toBeInstanceOf(Function);
+    const result = isIsogram;
+
+    expect(result)
+      .toBeInstanceOf(Function);
+  });
+
+  it(`should be case insensitive`, () => {
+    const result = isIsogram('Adam');
+
+    expect(result)
+      .toBe(false);
+  });
+
+  it(`should return true for isogram`, () => {
+    const result = isIsogram('playgrounds');
+
+    expect(result)
+      .toBe(true);
+  });
+
+  it(`should return true for empty string`, () => {
+    const result = isIsogram('');
+
+    expect(result)
+      .toBe(true);
+  });
+
+  it(`should return false for repeting letters`, () => {
+    const result = isIsogram('look');
+
+    expect(result)
+      .toBe(false);
   });
 });
