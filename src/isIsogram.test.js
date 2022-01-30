33e08674fd4138ff77cb44e6f50250d22795eaac
OleksandrLisnevskyi <lisnevskyi@gmail.com>
2022-01-30 13:21:27 +0200

    Add solution for isIsogram task

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,40 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return a boolean type`, () => {
+    expect(typeof isIsogram('sport')).toBe('boolean');
+  });
+
+  it(`should return a true if word === ''`, () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
+
+  it(`should return a true if word.length === 1`, () => {
+    expect(isIsogram('A')).toBeTruthy();
+  });
+
+  it(`should not be case sensitive`, () => {
+    expect(isIsogram('Abba')).toBeFalsy();
+  });
+
+  it(`should return false if word === 'aA'`, () => {
+    expect(isIsogram('aA')).toBe(false);
+  });
+
+  it(`should return true if word === 'isogram'`, () => {
+    expect(isIsogram('isogram')).toBe(true);
+  });
+
+  it(`should return false if word === 'my isogram'`, () => {
+    expect(isIsogram('my isogram')).toBe(false);
+  });
+
+  it(`should return true if word === 'unpredictably'`, () => {
+    expect(isIsogram('unpredictably')).toBe(true);
+  });
+
+  it(`should return false if word === 'unprEdictable'`, () => {
+    expect(isIsogram('unprEdictable')).toBe(false);
+  });
 });
