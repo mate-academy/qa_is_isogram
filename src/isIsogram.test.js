704817e81d555c6cc500b8a88fca1792d9444a56
Roman Stas <romastas1996@gmail.com>
2022-04-16 18:57:57 +0300

    solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,39 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return boolean`, () => {
+    const result = typeof (isIsogram('Ff'));
+
+    expect(result)
+      .toBe('boolean');
+  });
+
+  it(`should return true when string empty`, () => {
+    const result = isIsogram('');
+
+    expect(result)
+      .toBe(true);
+  });
+
+  it(`should return true when no repeating letters`, () => {
+    const result = isIsogram('playgrounds');
+
+    expect(result)
+      .toBe(true);
+  });
+
+  it(`should return false when letters is repeat`, () => {
+    const result = isIsogram('look');
+
+    expect(result)
+      .toBe(false);
+  });
+
+  it(`should return false when repeating in different registers`, () => {
+    const result = isIsogram('Adam');
+
+    expect(result)
+      .toBe(false);
+  });
 });
