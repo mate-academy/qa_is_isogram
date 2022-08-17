cb82394a524904ec77154452e915cb5807954008
Dasha Protsko <protscodasha@icloud.com>
2022-08-17 16:59:28 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,34 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`the empty string should be an isogram`, () => {
+    expect(isIsogram(' '))
+      .toBeTruthy();
+  });
+
+  it(`the 'playgrounds' string should be an isogram`, () => {
+    expect(isIsogram('playgrounds'))
+      .toBeTruthy();
+  });
+
+  it(`the 'Sun' string should be an isogram`, () => {
+    expect(isIsogram('playgrounds'))
+      .toBeTruthy();
+  });
+
+  it(`the 'aPple' string shouldn't  be an isogram`, () => {
+    expect(isIsogram('aPple'))
+      .toBeFalsy();
+  });
+
+  it(`the 'Madam' string shouldn't  be an isogram`, () => {
+    expect(isIsogram('aPple'))
+      .toBeFalsy();
+  });
+
+  it(`the 'moon' string shouldn't  be an isogram`, () => {
+    expect(isIsogram('aPple'))
+      .toBeFalsy();
+  });
 });
