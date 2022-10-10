d3b436e015c5fe87339cb7468deba69ff99e7c64
Dmytro Semenov <Dmytro.Semenov@tx.local>
2022-10-10 22:10:37 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,18 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should return true if comes an empty string', () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
+
+  it('should return correct value if a non-empty string is received', () => {
+    expect(isIsogram('playgrounds')).toBeTruthy();
+    expect(isIsogram('look')).toBeFalsy();
+  });
+  
+  it('should return a valid string regardless of case', () => {
+    expect(isIsogram('LooK')).toBeFalsy();
+    expect(isIsogram('PlayGrouNDS')).toBeTruthy();
+  });
 });
