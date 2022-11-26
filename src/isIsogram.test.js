9a9717c8b5fb543c9ddca9e39120b10fa1bc93a5
Larysa Krynytska <larisa.k.avramenko@gmail.com>
2022-11-26 20:35:35 +0200

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,35 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  test(`should return 'true' if input is empty`, () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
+
+  test(`should return 'true' if input has no duplicate letters`, () => {
+    expect(isIsogram('cat')).toBeTruthy();
+  });
+
+  test(`should return 'true' if input has 2 words and no 
+  duplicates letters`, () => {
+    expect(isIsogram('hi, cat')).toBeTruthy();
+  });
+
+  test(`should return 'false' if input has duplicates letters`, () => {
+    expect(isIsogram('coca')).toBeFalsy();
+  });
+
+  test(`should return 'false' if input has duplicates letters of 
+  different case`, () => {
+    expect(isIsogram('Bob')).toBeFalsy();
+  });
+
+  test(`should return 'false' if input has more than one space`, () => {
+    expect(isIsogram('You like cats')).toBeFalsy();
+  });
+
+  test(`should return 'false' if input has more than one special 
+  character`, () => {
+    expect(isIsogram('hi, cat,')).toBeFalsy();
+  });
 });
