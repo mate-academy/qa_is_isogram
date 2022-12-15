e60cc2108a9601e2ff2bc6e15fad4f6a1f15934f
Kyrylo Stadnyk <faust.ogr8@gmail.com>
2022-12-15 21:28:12 +0200

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -1,9 +1,41 @@
+/* eslint-disable max-len */
 'use strict';
 
 describe('isIsogram', () => {
   const { isIsogram } = require('./isIsogram');
 
   it(`should be declared`, () => {
-    expect(isIsogram).toBeInstanceOf(Function);
+    expect(isIsogram)
+      .toBeInstanceOf(Function);
+  });
+
+  test(`should return true if word is empty`, () => {
+    expect(isIsogram(''))
+      .toBe(true);
+  });
+
+  test(`should be case-insensitive`, () => {
+    expect(isIsogram('Abra'))
+      .toBe(false);
+  });
+
+  test(`should return true if word is isogram`, () => {
+    expect(isIsogram('main'))
+      .toBe(true);
+  });
+
+  test(`should return false if word has the same letters`, () => {
+    expect(isIsogram('banana'))
+      .toBe(false);
+  });
+
+  test(`should return true if word contain only space`, () => {
+    expect(isIsogram(' '))
+      .toBe(true);
+  });
+
+  test(`should return true if word contain the same Latin and Cyrillic letters`, () => {
+    expect(isIsogram('CatСобака'))
+      .toBe(false);
   });
 });
