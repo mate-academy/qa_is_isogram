adad682286225e1b759696f725d55b2aaf8a2a23
Bohdan Zaborovets <bogdan2013live@gmail.com>
2022-12-18 21:50:24 +0200

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return 'true' for empty string`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`should return 'false' for repeating letters`, () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it(`should be case insensitive`, () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it(`should 'true' for isogram`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
 });
