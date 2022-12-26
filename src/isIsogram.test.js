fe28fb553747f565a1a154132b48a22473338ced
Usenko Bohdana <usenkobogdana19@gmail.com>
2022-12-26 13:08:55 +0200

    solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return 'true' for empty string`, () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
+
+  it(`should return 'false' for repeating letters`, () => {
+    expect(isIsogram('good')).toBeFalsy();
+  });
+
+  it('should be case-insensitive', () => {
+    expect(isIsogram('Aaron')).toBeFalsy();
+  });
+
+  it(`should be 'true' for isogram`, () => {
+    expect(isIsogram('love')).toBeTruthy();
+  });
 });
