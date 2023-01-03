b224fc0243014ecbebca7bd6556d3670f9cebc3c
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2023-01-03 18:58:59 +0200

    isogram tests was implemented

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,32 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  describe('true', () => {
+    it(`word with no repeating letters`, () => {
+      const result = isIsogram('playgrounds');
+
+      expect(result).toBe(true);
+    });
+
+    it(`empty string`, () => {
+      const result = isIsogram('');
+
+      expect(result).toBe(true);
+    });
+  });
+
+  describe('false', () => {
+    it(`word with repeating letters`, () => {
+      const result = isIsogram('look');
+
+      expect(result).toBe(false);
+    });
+
+    it(`word with repeating letters but different case`, () => {
+      const result = isIsogram('Adam');
+
+      expect(result).toBe(false);
+    });
+  });
 });
