502d0003dbcf0a084092e239ae512798beea52c1
Andrii Dydyk <andriidydyk622@gmail.com>
2023-01-13 20:05:35 +0100

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -3,7 +3,44 @@
 describe('isIsogram', () => {
   const { isIsogram } = require('./isIsogram');
 
-  it(`should be declared`, () => {
+  test(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
+  });
+
+  test(`return 'true' if argument equel empty string`, () => {
+    const isogram = isIsogram('');
+
+    expect(isogram)
+      .toEqual(true);
+  });
+
+  test(`return 'true' if argument contains no repeating letters`, () => {
+    const isogram = isIsogram('abcdef');
+
+    expect(isogram)
+      .toEqual(true);
+  });
+
+  test(`return 'true' if argument contains no repeating`
+  + `and case-insensitive letters`, () => {
+    const isogram = isIsogram('aBcDeF');
+
+    expect(isogram)
+      .toEqual(true);
+  });
+
+  test(`return 'false' if argument contains repeating letters`, () => {
+    const isogram = isIsogram('aabcdef');
+
+    expect(isogram)
+      .toEqual(false);
+  });
+
+  test(`return 'false' if argument contains repeating`
+  + `and case-insensitive letters`, () => {
+    const isogram = isIsogram('Aabcdef');
+
+    expect(isogram)
+      .toEqual(false);
   });
 });
