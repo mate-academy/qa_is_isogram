bd2bf20b983e178dd187d335a91d3f2c1be2a3fd
Khrystyna Sabat <satttinka@gmail.com>
2023-02-02 21:44:52 +0100

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,34 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true for an isogram`, () => {
+    expect(isIsogram('playgrounds'))
+      .toBe(true);
+  });
+
+  it(`should return true for empty string`, () => {
+    expect(isIsogram(''))
+      .toBe(true);
+  });
+
+  it(`should return false if the word has 2 the same letters`, () => {
+    expect(isIsogram('look'))
+      .toBe(false);
+  });
+
+  it(`should return false if the word has 3 the same letters`, () => {
+    expect(isIsogram('lookout'))
+      .toBe(false);
+  });
+
+  it(`should return false if several letters repeat`, () => {
+    expect(isIsogram('attention'))
+      .toBe(false);
+  });
+
+  it(`should return false if the same letters in different registers`, () => {
+    expect(isIsogram('Adam'))
+      .toBe(false);
+  });
 });
