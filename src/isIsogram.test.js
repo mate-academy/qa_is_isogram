2d3c247c122448a3022f9ae6d05d80b7c758a7fc
E Fernandez <ernesto.fernandez.1980@gmail.com>
2023-02-23 21:34:25 +0200

    solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,32 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`be true for ''`, () => {
+    const word = '';
+
+    expect(isIsogram(word))
+      .toBe(true);
+  });
+
+  it(`be true for one symbol`, () => {
+    const word = 'a';
+
+    expect(isIsogram(word))
+      .toBe(true);
+  });
+
+  it(`be false for m&m`, () => {
+    const word = 'mM';
+
+    expect(isIsogram(word))
+      .toBe(false);
+  });
+
+  it(`be false for m&M`, () => {
+    const word = 'mM';
+
+    expect(isIsogram(word))
+      .toBe(false);
+  });
 });
