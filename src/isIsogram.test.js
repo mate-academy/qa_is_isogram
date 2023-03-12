4f51ba406678ec25561e576a346626e7712aca32
Maryna Oliinyk <ukrmartin22@gmail.com>
2023-03-12 17:41:39 +0200

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,29 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return a boolean`, () => {
+    expect(typeof isIsogram('playgrounds'))
+      .toBe('boolean');
+  });
+
+  it(`should return 'true' when input is 'playgrounds'`, () => {
+    expect(isIsogram('playgrounds'))
+      .toBe(true);
+  });
+
+  it(`should return 'true' when input is ''`, () => {
+    expect(isIsogram(''))
+      .toBe(true);
+  });
+
+  it(`should return 'false' when input is 'look'`, () => {
+    expect(isIsogram('look'))
+      .toBe(false);
+  });
+
+  it(`should return 'false' when input is 'Adam'`, () => {
+    expect(isIsogram('Adam'))
+      .toBe(false);
+  });
 });
