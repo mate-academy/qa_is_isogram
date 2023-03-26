f9893754a800bc9108e58300a60f9aefff3da67d
Max Nikiforov <maxxnikiforov@gmail.com>
2023-03-26 21:11:25 +0700

    tests added

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,24 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should return true if all letters are uniq', () => {
+    expect(isIsogram('playgrounds'))
+      .toEqual(true);
+  });
+
+  it('should return false if letters are not uniq', () => {
+    expect(isIsogram('playgroundsp'))
+      .toEqual(false);
+  });
+
+  it('should return false if exist same letters in diff cases', () => {
+    expect(isIsogram('Apart'))
+      .toEqual(false);
+  });
+
+  it(`should return true if word = ''`, () => {
+    expect(isIsogram(''))
+      .toEqual(true);
+  });
 });
