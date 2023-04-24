a6584b129e58a42613331ccf986537fc2abdb65f
Anastasiia Zheliaskova <a.zheliaskova3@gmail.com>
2023-04-24 21:13:33 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,47 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true if users nothing enters`, () => {
+    expect(isIsogram(''))
+      .toBeTruthy();
+  });
+
+  it(`should return true if users enters isogram`, () => {
+    expect(isIsogram('beach'))
+      .toBeTruthy();
+  });
+
+  it(`should return false if users enters repeating letters`, () => {
+    expect(isIsogram('toothpick'))
+      .toBeFalsy();
+  });
+
+  it(`should return false if users enters word with 
+    identical letters in different cases`, () => {
+    expect(isIsogram('toOthpick'))
+      .toBeFalsy();
+  });
+
+  it(`should return false if users enters word repeating 
+    consecutive parts`, () => {
+    expect(isIsogram('bilabila'))
+      .toBeFalsy();
+  });
+
+  it(`should return true if users enters one letter`, () => {
+    expect(isIsogram('q'))
+      .toBeTruthy();
+  });
+
+  it(`should return true if users enters long isogram`, () => {
+    expect(isIsogram('abcdefghijklmnopqrstuvwxyz'))
+      .toBeTruthy();
+  });
+
+  it(`should return true if users enters two identical letters 
+    but different alphabets`, () => {
+    expect(isIsogram('aа'))
+      .toBeTruthy();
+  });
 });
