009fbd9eabbcbc3f03391afa228714b5baaa9906
yuliiapyrih <yuliia.pyrih.pm.2018@lpnu.ua>
2023-04-25 11:30:06 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,27 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true 
+    if word is an isogram`, () => {
+    expect(isIsogram(''))
+      .toBeTruthy();
+  });
+
+  it(`should return true 
+    if word is empty`, () => {
+    expect(isIsogram(''))
+      .toBeTruthy();
+  });
+
+  it(`should be case-insensitive`, () => {
+    expect(isIsogram('Adam'))
+      .toBeFalsy();
+  });
+
+  it(`should return true 
+  if word is not isogram`, () => {
+    expect(isIsogram('look'))
+      .toBeFalsy();
+  });
 });
