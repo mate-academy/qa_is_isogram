8d3ac2a80f3d312e4900b9f5bb5354f8c0e8e4ef
Anastasiia Zheliaskova <a.zheliaskova3@gmail.com>
2023-04-26 11:15:44 +0300

    Fixes after code review

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -7,45 +7,45 @@ describe('isIsogram', () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
 
-  it(`should return true if users nothing enters`, () => {
+  it(`should return true if an empty string is entered`, () => {
     expect(isIsogram(''))
       .toBeTruthy();
   });
 
-  it(`should return true if users enters isogram`, () => {
+  it(`should return true if an isogram is entered`, () => {
     expect(isIsogram('beach'))
       .toBeTruthy();
   });
 
-  it(`should return false if users enters repeating letters`, () => {
+  it(`should return false if a word with repeating letters is entered`, () => {
     expect(isIsogram('toothpick'))
       .toBeFalsy();
   });
 
-  it(`should return false if users enters word with 
-    identical letters in different cases`, () => {
+  it(`should return false if a word with identical letters 
+    in different cases is entered`, () => {
     expect(isIsogram('toOthpick'))
       .toBeFalsy();
   });
 
-  it(`should return false if users enters word repeating 
-    consecutive parts`, () => {
+  it(`should return false if a word with repeating consecutive 
+    parts is entered`, () => {
     expect(isIsogram('bilabila'))
       .toBeFalsy();
   });
 
-  it(`should return true if users enters one letter`, () => {
+  it(`should return true if a single letter is entered`, () => {
     expect(isIsogram('q'))
       .toBeTruthy();
   });
 
-  it(`should return true if users enters long isogram`, () => {
+  it(`should return true if a long isogram is entered`, () => {
     expect(isIsogram('abcdefghijklmnopqrstuvwxyz'))
       .toBeTruthy();
   });
 
-  it(`should return true if users enters two identical letters 
-    but different alphabets`, () => {
+  it(`should return true if two identical letters from 
+    different alphabets are entered`, () => {
     expect(isIsogram('aа'))
       .toBeTruthy();
   });
