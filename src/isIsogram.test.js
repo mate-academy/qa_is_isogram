e56fdcbd2d83d6cf49d4e881bcc1e2f83a3eab7a
Artem Sukhetskiy <artem.sukhetskiy@mirko.in.ua>
2023-05-12 09:34:37 +0300

    solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,39 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`'word' has 1 repeating letter`, () => {
+    const actual = isIsogram('dad');
+    const expected = false;
+
+    expect(actual).toBe(expected);
+  });
+
+  it(`'word' has no repeating letters`, () => {
+    const actual = isIsogram('mate');
+    const expected = true;
+
+    expect(actual).toBe(expected);
+  });
+
+  it(`'word' has 3 repeating letter`, () => {
+    const actual = isIsogram('Innkeepers');
+    const expected = false;
+
+    expect(actual).toBe(expected);
+  });
+
+  it(`'word' is empty string`, () => {
+    const actual = isIsogram('');
+    const expected = true;
+
+    expect(actual).toBe(expected);
+  });
+
+  it(`'word' has 1 upper case repeating letter`, () => {
+    const actual = isIsogram('Lol');
+    const expected = false;
+
+    expect(actual).toBe(expected);
+  });
 });
