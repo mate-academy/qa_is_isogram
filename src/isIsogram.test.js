cb0528026de3035d9bde4180262ac72c7f1de2a5
Oleh Chermak <ochermak2002@gmail.com>
2023-05-15 11:37:25 +0300

    solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,29 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should return true for empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it('should return false if word is not isogram', () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it('should return true if word is isogram', () => {
+    expect(isIsogram('products')).toBe(true);
+  });
+
+  it('should work with uppercase word', () => {
+    expect(isIsogram('PRODUCTS')).toBe(true);
+  });
+
+  it('should be case-insensitive', () => {
+    expect(isIsogram('PlAyGroUnDS')).toBe(true);
+  });
+
+  it('should return false if word includes one'
+   + ' letter in both upper and lower cases', () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
 });
