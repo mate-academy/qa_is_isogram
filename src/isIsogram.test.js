a3e0cb0b91c41d0acc9d98822c5227ebc74a056a
Artem Sukhetskiy <artem.sukhetskiy@mirko.in.ua>
2023-05-16 16:25:54 +0300

    Changed

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -7,35 +7,35 @@ describe('isIsogram', () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
 
-  it(`'word' has 1 repeating letter`, () => {
+  it(`should return false if 'word' has 1 repeating letter`, () => {
     const actual = isIsogram('dad');
     const expected = false;
 
     expect(actual).toBe(expected);
   });
 
-  it(`'word' has no repeating letters`, () => {
+  it(`should return true if 'word' has no repeating letters`, () => {
     const actual = isIsogram('mate');
     const expected = true;
 
     expect(actual).toBe(expected);
   });
 
-  it(`'word' has 3 repeating letter`, () => {
+  it(`should return false if 'word' has 3 repeating letter`, () => {
     const actual = isIsogram('Innkeepers');
     const expected = false;
 
     expect(actual).toBe(expected);
   });
 
-  it(`'word' is empty string`, () => {
+  it(`should return true if 'word' is empty string`, () => {
     const actual = isIsogram('');
     const expected = true;
 
     expect(actual).toBe(expected);
   });
 
-  it(`'word' has 1 upper case repeating letter`, () => {
+  it(`should return false if 'word' has 1 upper case repeating letter`, () => {
     const actual = isIsogram('Lol');
     const expected = false;
 
