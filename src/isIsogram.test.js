8d09add6092f4d8230ba0491f04c030a52e1a206
Yurii Demkiv <demkiv123@gmail.com>
2023-05-17 19:08:10 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,24 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true for empty string`, () => {
+    expect(isIsogram(''))
+      .toBe(true);
+  });
+
+  it(`should return true for valid isogram`, () => {
+    expect(isIsogram('mate'))
+      .toBe(true);
+  });
+
+  it(`should return false for an invalid isogram`, () => {
+    expect(isIsogram('academy'))
+      .toBe(false);
+  });
+
+  it(`should be case insensitive`, () => {
+    expect(isIsogram('maTE'))
+      .toBe(true);
+  });
 });
