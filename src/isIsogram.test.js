ec711f0285b9d8b924053498b010dd129c348ec2
Ruslan Vrazovskyi <dnkons@gmail.com>
2023-06-22 21:14:24 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -15,30 +15,26 @@ describe('isIsogram', () => {
     expect(isIsogram('playgrounds')).toBe(true);
   });
 
-  it('should return false for a word with repeating letters',
-    () => {
-      expect(isIsogram('look')).toBe(false);
-    });
+  it('should return false for a word with repeating letters', () => {
+    expect(isIsogram('look')).toBe(false);
+  });
 
   it('should be case-insensitive', () => {
     expect(isIsogram('Adam')).toBe(false);
     expect(isIsogram('madam')).toBe(false);
   });
 
-  it('should return true for a word with no repeating letters',
-    () => {
-      expect(isIsogram('abcdefg')).toBe(true);
-    });
+  it('should return true for a word with no repeating letters', () => {
+    expect(isIsogram('abcdefg')).toBe(true);
+  });
 
-  it('should return false for a word with consecutive repeating letters',
-    () => {
-      expect(isIsogram('hello')).toBe(false);
-    });
+  it('should return false for a word with consecutive repeating letters', () => {
+    expect(isIsogram('hello')).toBe(false);
+  });
 
-  it('should return true for a word with non-consecutive repeating letters',
-    () => {
-      expect(isIsogram('yellow')).toBe(false);
-    });
+  it('should return false for a word with non-consecutive repeating letters', () => {
+    expect(isIsogram('yellow')).toBe(false);
+  });
 
   it('should return true for a word with all the same letters', () => {
     expect(isIsogram('aaa')).toBe(false);
