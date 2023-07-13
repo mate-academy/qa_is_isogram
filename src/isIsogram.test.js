cc892ca4e85e0a78089dd8ea4124d3e62ffef219
Dmytro Budynskyi <dbudinskyy@gmail.com>
2023-07-13 15:49:29 +0200

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it('should be case-insensitive', () => {
+    expect(isIsogram('isisOGraM')).toBe(false);
+  });
+
+  it('Word without duplicated character', () => {
+    expect(isIsogram('twelve')).toBe(false);
+  });
+
+  it('Word with duplicated character in mixed case', () => {
+    expect(isIsogram('Alphabet')).toBe(false);
+  });
 });
