12068d6368e260aec1706734952313af1c59d59c
Oselskyi Oleksandr <oselskyi3@gmail.com>
2023-07-23 10:49:36 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,24 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return boolean`, () => {
+    expect(isIsogram('watch')).toEqual(expect.any(Boolean));
+  });
+
+  it(`should return true if params is an emty string`, () => {
+    expect(isIsogram('')).toEqual(true);
+  });
+
+  it(`should return true if params has no repeating letters`, () => {
+    expect(isIsogram('playgrounds')).toEqual(true);
+  });
+
+  it(`should return false if params has repeating letters`, () => {
+    expect(isIsogram('playgrounds')).toEqual(true);
+  });
+
+  it(`should be case-insensitive`, () => {
+    expect(isIsogram('Adam')).toEqual(false);
+  });
 });
