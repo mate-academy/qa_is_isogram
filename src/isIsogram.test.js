9b171b0701a76ddcb9573157563a20f74cbf99b4
Olga Zaiets <zayets.olga@gmail.com>
2023-07-26 16:15:39 +0200

    added test cases to test.js file

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,28 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true with empty string `, () => {
+    const result = isIsogram('');
+
+    expect(result).toBeTruthy();
+  });
+
+  it(`should be case-insensitive`, () => {
+    const result = isIsogram('Oo');
+
+    expect(result).toBeFalsy();
+  });
+
+  it(`should be isogram with word 'pencil'`, () => {
+    const result = isIsogram('pencil');
+
+    expect(result).toBeTruthy();
+  });
+
+  it(`should be not isogram with word 'book'`, () => {
+    const result = isIsogram('book');
+
+    expect(result).toBeFalsy();
+  });
 });
