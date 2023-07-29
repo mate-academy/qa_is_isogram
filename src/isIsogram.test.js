977dfc1d49cd6ddae15fc9b57c73d28f0d127605
Mykhailo Dzhemula <mykhailo.dzhemula@nordstrom.com>
2023-07-29 22:32:17 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,24 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should return "true" for an empty string', () => {
+    expect(isIsogram(''))
+      .toEqual(true);
+  });
+
+  it('should be case-insensitive', () => {
+    expect(isIsogram('Adam'))
+      .toEqual(false);
+  });
+
+  it('should return "true" for word that has no repeating letters', () => {
+    expect(isIsogram('playgrounds'))
+      .toEqual(true);
+  });
+
+  it('should return "false" for word that has repeating letters', () => {
+    expect(isIsogram('look'))
+      .toEqual(false);
+  });
 });
