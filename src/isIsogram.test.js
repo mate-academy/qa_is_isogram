7d947cb023a26aadfd36ef727058254b1bcd0e84
Vadym Dubniak <dubnyakv@gmail.com>
2023-08-09 17:19:31 +0300

    implemented tests

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,39 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should throw an error on invalid arguments type`, () => {
+    expect(() => {
+      isIsogram(123);
+    })
+      .toThrow();
+  });
+
+  it(`should return 'true' for empty string`, () => {
+    const result = isIsogram('');
+
+    expect(result)
+      .toBe(true);
+  });
+
+  it(`should return 'true' for isogram`, () => {
+    const result = isIsogram('123');
+
+    expect(result)
+      .toBe(true);
+  });
+
+  it(`should return 'false' for not isogram`, () => {
+    const result = isIsogram('12333');
+
+    expect(result)
+      .toBe(false);
+  });
+
+  it(`should return correct value regardles of case`, () => {
+    const result = isIsogram('Aa');
+
+    expect(result)
+      .toBe(false);
+  });
 });
