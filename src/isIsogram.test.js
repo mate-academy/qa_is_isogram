ef60ee7338fe42e51b6c664fd827e1b2d876fcde
BohdanBondarchuk <bogdangalanet@gmail.com>
2023-08-10 22:50:05 +0300

    Final work

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,19 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should be (true) if word is (isogram)`, () => {
+    expect(isIsogram('isogram'))
+      .toBe(true);
+  });
+
+  it(`should be (false) if word is (isSogram)`, () => {
+    expect(isIsogram('isSogram'))
+      .toBe(false);
+  });
+
+  it(`should be (true) if word is (empty)`, () => {
+    expect(isIsogram(''))
+      .toBe(true);
+  });
 });
