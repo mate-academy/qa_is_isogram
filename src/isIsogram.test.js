c66dc427d91e256b0c5447ff09c663cc305f500a
Svitlana Zai <svetkaqq1991@gmail.com>
2023-08-13 17:38:49 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,34 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should retutrn 'true' `
+  + `if 'word' is empty 'string'`, () => {
+    expect(isIsogram(''))
+      .toBe(true);
+  });
+
+  it(`should retutrn 'true' `
+  + `if 'word' is isogram with lowercase letters`, () => {
+    expect(isIsogram('playgrounds'))
+      .toBe(true);
+  });
+
+  it(`should retutrn 'true' `
+  + `if 'word' is isogram with mixed-case letters`, () => {
+    expect(isIsogram('GarDen'))
+      .toBe(true);
+  });
+
+  it(`should retutrn 'false' `
+  + `if 'word' is not isogram with lowercase letters`, () => {
+    expect(isIsogram('look'))
+      .toBe(false);
+  });
+
+  it(`should retutrn 'false' `
+  + `if 'word' is not isogram with mixed-case letters`, () => {
+    expect(isIsogram('Adam'))
+      .toBe(false);
+  });
 });
