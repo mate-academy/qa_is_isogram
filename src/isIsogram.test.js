d8b8ec1cf1165f8e06d4d76bc2aebb6286a88ef3
Mykhailo Kostov <kostov.mykhailo@gmail.com>
2023-08-27 13:53:14 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`'true' if has no repeating letters`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it(`'false' if has repeating letters`, () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it(`'false' if has repeating capital letters`, () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it(`'true' for empty argument`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
 });
