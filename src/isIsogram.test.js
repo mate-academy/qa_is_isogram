d5ed9f586d28cd47efd28dec4b6533953f51483b
Bohdan Svystun <bogdansvystun02@gmail.com>
2023-09-11 19:55:23 +0300

    Second commit

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -3,37 +3,31 @@
 describe('isIsogram', () => {
   const { isIsogram } = require('./isIsogram');
 
-  describe('`isIsogram` function', () => {
-    it(`should be declared`, () => {
-      expect(isIsogram).toBeInstanceOf(Function);
-    });
+  it(`should be declared`, () => {
+    expect(isIsogram).toBeInstanceOf(Function);
   });
 
-  describe('Empty braces', () => {
-    it(`should be an isogram`, () => {
-      expect(isIsogram('')).toBe(true);
-    });
+  it(`should be an isogram when passed an empty string`, () => {
+    expect(isIsogram('')).toBe(true);
   });
 
-  describe('`isIsogram` function is case-insensitive', () => {
-    it(`should be case-insensitive`, () => {
-      expect(isIsogram('Adam')).toBe(false);
-      expect(isIsogram('Momo')).toBe(false);
-    });
+  it(`should be case-insensitive`, () => {
+    expect(isIsogram('Momo')).toBe(false);
   });
 
-  describe('`false` cases', () => {
-    it('should return false for non-isograms', () => {
-      expect(isIsogram('look')).toBe(false);
-      expect(isIsogram('mama')).toBe(false);
-    });
+  it(`should be case-insensitive`, () => {
+    expect(isIsogram('Adam')).toBe(false);
   });
 
-  describe('`true` cases', () => {
-    it('should return true for isograms', () => {
-      expect(isIsogram('Lock')).toBe(true);
-      expect(isIsogram('Venom')).toBe(true);
-      expect(isIsogram('playground')).toBe(true);
-    });
+  it('should return false for non-isograms', () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it('should return false for non-isograms', () => {
+    expect(isIsogram('mama')).toBe(false);
+  });
+
+  it('should return true for isograms', () => {
+    expect(isIsogram('playground')).toBe(true);
   });
 });
