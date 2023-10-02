80e3395ebb0e86dd4d342a8c374ebb2cc4767048
Snizhana Broniar <sky.fantasy66@gmail.com>
2023-10-03 00:13:26 +0300

    Corrected

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -12,15 +12,35 @@ describe('isIsogram', () => {
       .toBeTruthy();
   });
 
-  it('should return true when input is "mother"', () => {
-    expect(isIsogram('mother'))
-      .toBeTruthy();
-  });
+  it('should return true when input is an isogram with lowercase letters',
+    () => {
+      expect(isIsogram('mother'))
+        .toBeTruthy();
+    });
 
-  it('should return true when input is "Father"', () => {
-    expect(isIsogram('Father'))
-      .toBeTruthy();
-  });
+  it('should return true when input is isogram with uppercase lowercase letters'
+    , () => {
+      expect(isIsogram('Father'))
+        .toBeTruthy();
+    });
+
+  it('should return true when input is lowercase isogram + number'
+    , () => {
+      expect(isIsogram('card1'))
+        .toBeTruthy();
+    });
+
+  it('should return true when input is Uppercase isogram + number'
+    , () => {
+      expect(isIsogram('1Home'))
+        .toBeTruthy();
+    });
+
+  it('should return true when input is lowercase isogram + special ch'
+    , () => {
+      expect(isIsogram('$witch'))
+        .toBeTruthy();
+    });
 
   it('should return false when input is "Hello"', () => {
     expect(isIsogram('Hello'))
