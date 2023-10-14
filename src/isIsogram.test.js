81b83b3f4d7b2eafb974450d6c06f28ce3caa5f3
Olesia Semen <semenolesa01@gmail.com>
2023-10-15 00:23:07 +0300

    Testing

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+  it('should return true for an empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it('should return true for an isogram word with mixed case', () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it('should return false for a word with repeating letters', () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it('should return false for a word with repeating letters (case-insensitive)', () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
 });
