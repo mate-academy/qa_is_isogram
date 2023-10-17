3326c5dacf16d81ae32307da4158bb2c0cce98b3
Artem Kravets <kravetsartyom@gmail.com>
2023-10-17 14:26:27 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return 'true' when the word is isogram`, () => {
+    expect(isIsogram('playgrounds')).toBeTruthy();
+  });
+
+  it(`should return 'false' when more than 1 letter repeats`, () => {
+    expect(isIsogram('look')).toBeFalsy();
+  });
+
+  it(`should return 'false' when the letter repeats in uppercase`, () => {
+    expect(isIsogram('Adam')).toBeFalsy();
+  });
+
+  it(`should return 'true' when there is no input`, () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
 });
