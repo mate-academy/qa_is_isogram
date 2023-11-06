421a136d3ac91c55123b9847b393cdc10bf59024
Mikhail Koponkin <miponkin@gmail.com>
2023-11-06 17:26:18 +0200

    IItest

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should return true for valid isogram', () => {
+    expect(isIsogram('head')).toBeTruthy();
+  });
+
+  it('should return false for an invalid isogram', () => {
+    expect(isIsogram('footer')).toBeFalsy();
+  });
+
+  it('should be case insensitive', () => {
+    expect(isIsogram('CorE')).toBeTruthy();
+  });
+
+  it('should return true for empty string', () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
 });
