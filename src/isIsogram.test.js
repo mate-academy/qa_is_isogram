4a18e67681775cb4edc4502bab6f4999761d061b
VladSlob <141657806+VladSlob@users.noreply.github.com>
2023-11-17 07:27:59 +0200

    isIsogram.test.js

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true for an empty string`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`should return true for a word with no repeating letters`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it(`should return false for a word with repeating letters`, () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it(`should be case-insensitive`, () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
 });
