426f20ff50059923708f69f99970ef4f4ca0bf49
AnatoliiDomin <talleer81@gmail.com>
2023-11-26 20:11:38 +0200

    add isogram test

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,26 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true for isogram words`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it(`should return true for the empty string`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`should return true for isogram words`
+  + `which includes upper letters`, () => {
+    expect(isIsogram('Home')).toBe(true);
+  });
+
+  it(`should return false for non-isogram words`, () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it(`should return false for non-isogram words`
+  + `which includes upper letters`, () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
 });
