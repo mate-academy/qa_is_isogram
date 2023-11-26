755c9d207f342ff0bc7b41a2a0911398ee41055d
Dmytro Horodchuk <dmytro.horodchuk@gmail.com>
2023-11-26 18:33:06 +0200

    tests added

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,21 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('playgrounds', () => {
+    expect(isIsogram('playgrounds')).toBeTruthy();
+  });
+
+  it(' ', () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
+
+  it('look', () => {
+    expect(isIsogram('look')).toBeFalsy();
+  });
+
+  it('Adam', () => {
+    expect(isIsogram('Adam')).toBeFalsy();
+  });
 });
+
