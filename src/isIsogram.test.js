401fd7e586048186288941c2915e498cb0b33918
AnatoliiDomin <talleer81@gmail.com>
2023-11-27 13:14:18 +0200

    more tests and used .toBeTruthy

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -8,24 +8,42 @@ describe('isIsogram', () => {
   });
 
   it(`should return true for isogram words`, () => {
-    expect(isIsogram('playgrounds')).toBe(true);
+    expect(isIsogram('playgrounds')).toBeTruthy();
   });
 
   it(`should return true for the empty string`, () => {
-    expect(isIsogram('')).toBe(true);
+    expect(isIsogram('')).toBeTruthy();
+  });
+
+  it(`should return true for a single letter string`, () => {
+    expect(isIsogram('а')).toBeTruthy();
+  });
+
+  it(`should return true for an isogram word with two letters`, () => {
+    expect(isIsogram('Ва')).toBeTruthy();
   });
 
   it(`should return true for isogram words`
   + `which includes upper letters`, () => {
-    expect(isIsogram('Home')).toBe(true);
+    expect(isIsogram('Home')).toBeTruthy();
+  });
+
+  it(`should return true for an isogram word`
+  + `with only uppercase letters`, () => {
+    expect(isIsogram('ВRLMS')).toBeTruthy();
   });
 
   it(`should return false for non-isogram words`, () => {
-    expect(isIsogram('look')).toBe(false);
+    expect(isIsogram('look')).toBeFalsy();
   });
 
   it(`should return false for non-isogram words`
   + `which includes upper letters`, () => {
-    expect(isIsogram('Adam')).toBe(false);
+    expect(isIsogram('Adam')).toBeFalsy();
+  });
+
+  it(`should return false for a non-isogram word`
+  + `with two uppercase letters`, () => {
+    expect(isIsogram('BB')).toBeFalsy();
   });
 });
