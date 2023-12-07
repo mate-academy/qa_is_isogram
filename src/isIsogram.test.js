d058271984cd3fa3086b99721d875271ee80b560
sheva-laptop <vladislavshevchenko10@gmail.com>
2023-12-07 19:27:56 +0200

    add solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -3,7 +3,31 @@
 describe('isIsogram', () => {
   const { isIsogram } = require('./isIsogram');
 
-  it(`should be declared`, () => {
+  it('should be declared', () => {
     expect(isIsogram).toBeInstanceOf(Function);
+  });
+
+  it('should return true for an empty string', () => {
+    const result = isIsogram('');
+
+    expect(result).toBe(true);
+  });
+
+  it('should return false for a non-isogram', () => {
+    const result = isIsogram('barcelona');
+
+    expect(result).toBe(false);
+  });
+
+  it('should return true for an isogram', () => {
+    const result = isIsogram('iphone');
+
+    expect(result).toBe(true);
+  });
+
+  it('should return false for a non-isogram with different case', () => {
+    const result = isIsogram('Snickers');
+
+    expect(result).toBe(false);
   });
 });
