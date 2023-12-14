7be487bfda52aa5cc0992ed70afb5057ae54bfe7
Aleksandr <cebanoleksandr@gmail.com>
2023-12-14 15:45:23 +0200

    solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,22 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true if there are no repeated letters`, () => {
+    expect(isIsogram('playgrounds')).toBeTruthy();
+  });
+
+  it(`should return false if there are repeated letters`, () => {
+    expect(isIsogram('look')).toBeFalsy();
+  });
+
+  it(
+    `should return false if there are 2 the same letters but in other cases`,
+    () => {
+      expect(isIsogram('Adam')).toBeFalsy();
+    });
+
+  it(`should return true if for empty string`, () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
 });
