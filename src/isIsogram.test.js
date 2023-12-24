b341789679f0a6a72e78b982add38e4d5525d9b8
Nazar <nazarorlov552@gmail.com>
2023-12-25 00:27:11 +0200

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`Should return 'true' with 'playgrounds' imput`, () => {
+    expect(isIsogram('playgrounds')).toEqual(true);
+  });
+
+  it(`Should return 'false' with 'hii' imput`, () => {
+    expect(isIsogram('hii')).toEqual(false);
+  });
+
+  it(`Should return 'false' with 'Aa' imput`, () => {
+    expect(isIsogram('Aa')).toEqual(false);
+  });
+
+  it(`Should return 'true' with '' imput`, () => {
+    expect(isIsogram('hii')).toEqual(false);
+  });
 });
