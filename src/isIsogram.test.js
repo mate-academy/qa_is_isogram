e827c7e9119eb1cdf6f38f7e464fae798402aaaf
Yehor Korablin <yegor_ka2004@ukr.net>
2024-01-21 15:25:30 +0200

    Added tests

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,40 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should be true for empty input`, () => {
+    expect(isIsogram('')).toEqual(true);
+  });
+
+  it(`should be true for Abcd`, () => {
+    expect(isIsogram('Abcd')).toEqual(true);
+  });
+
+  it(`should be false for ABCabc`, () => {
+    expect(isIsogram('ABCabc')).toEqual(false);
+  });
+
+  it(`should be false for Adam`, () => {
+    expect(isIsogram('Adam')).toEqual(false);
+  });
+
+  it(`should be true for 1 space`, () => {
+    expect(isIsogram(' ')).toEqual(true);
+  });
+
+  it(`should be false for 2 spaces`, () => {
+    expect(isIsogram('  ')).toEqual(false);
+  });
+
+  it(`should be false for incorrect example`, () => {
+    expect(isIsogram('incorrect example')).toEqual(false);
+  });
+
+  it(`should be false for passed`, () => {
+    expect(isIsogram('passed')).toEqual(false);
+  });
+
+  it(`should be true for failed`, () => {
+    expect(isIsogram('failed')).toEqual(true);
+  });
 });
