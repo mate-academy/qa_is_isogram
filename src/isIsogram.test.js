ef56cc5f10c13a19d34f2f5701e5a9a772fbd208
Julia14012000 <Juliamilan1414@gmail.com>
2024-02-04 20:57:47 +0200

    Solution isIsogram

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,23 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`arg - word with without repetition of letters`, () => {
+    expect(isIsogram('hey')).toBe(true);
+  });
+
+  it(`arg - word with repetition of letters`, () => {
+    expect(isIsogram('Hello')).toBe(false);
+    expect(isIsogram('sOon')).toBe(false);
+  });
+
+  it(`arg - space`, () => {
+    expect(isIsogram(' ')).toBe(true);
+  });
+
+  it(`should handle error when input is empty or not valid)`, () => {
+    expect(() => isIsogram()).toThrow();
+    expect(() => isIsogram(4)).toThrow();
+    expect(() => isIsogram(NaN)).toThrow();
+  });
 });
