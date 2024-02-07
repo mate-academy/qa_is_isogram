68446e8222854c4ee91f50951af19b925a485310
Tetiana Maksiuk <TetianaMaksiuk@gmail.com>
2024-02-07 17:18:44 +0200

    Impliment tests for isIsogram

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should return true for an empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it('should return true for an isogram word "quality"', () => {
+    expect(isIsogram('quality')).toBe(true);
+  });
+
+  it('should return false for a non-isogram word "assurance"', () => {
+    expect(isIsogram('assurance')).toBe(false);
+  });
+
+  it('should return false for a non-isogram word "Tetiana"', () => {
+    expect(isIsogram('Tetiana')).toBe(false);
+  });
 });
