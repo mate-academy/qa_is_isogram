27e04bf2fec1330a0d695e3c188bc0dbe6d5b734
Maksym Akhtamov <maksimakhtamov10@gmail.com>
2024-02-07 15:44:16 +0200

    solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,16 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should work independently from Upper and Lower cases', () => {
+    expect(isIsogram('Maxik')).toBe(true);
+  });
+
+  it('should return false when the word is not an isogram', () => {
+    expect(isIsogram('Maximilian')).toBe(false);
+  });
+
+  it('should return true for an empty argument', () => {
+    expect(isIsogram('')).toBe(true);
+  });
 });
