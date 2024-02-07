cbefb53c1a955775380ec195254fd9ed42dc257b
Inna Levytska <inna.levytska24.com>
2024-02-07 15:50:30 +0200

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,16 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('Should return false for "look"', () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it('Should return false for "Adam"', () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it('Should return true for an empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
 });
