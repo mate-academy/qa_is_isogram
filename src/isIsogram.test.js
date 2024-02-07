ba46cc96bd919cf7ec64e2656f5eda650bb30296
oleksiibulgakov <bulgakov.oleksii@gmail.com>
2024-02-07 17:39:34 +0200

    Task solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,22 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should treat empty string as isogram`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`should treat Upper and Lower case as equal`, () => {
+    expect(isIsogram('Mom')).toBe(false);
+  });
+
+  it(`should return true on isograms`, () => {
+    expect(isIsogram('colman')).toBe(true);
+  });
+
+  it(`should return false for non isograms`, () => {
+    expect(isIsogram('Conan')).toBe(false);
+    expect(isIsogram('boo')).toBe(false);
+    expect(isIsogram('ffan')).toBe(false);
+  });
 });
