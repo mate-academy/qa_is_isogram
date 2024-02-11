77024d3d90c468e816f235f6de0e77a41bdbc0cf
Myself <wetball74@gmail.com>
2024-02-12 00:43:30 +0200

    new try

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -4,10 +4,24 @@ describe('isIsogram', () => {
   const { isIsogram } = require('./isIsogram');
 
   it(`should be declared`, () => {
-    expect(isIsogram).toBeInstanceOf(Function);
+    expect(isIsogram)
+      .toBeInstanceOf(Function);
   });
 
-  it(`should be declared`, () => {
-    expect(isIsogram).toBeInstanceOf(Function);
+  it(`Empty line should return true`, () => {
+    expect(isIsogram(''))
+      .toBe(true);
+  });
+
+  it(`String that has no repeating letters, case-insensetive, should return true`, () => {
+    expect(isIsogram('playgrounds'))
+      .toBe(true);
+  });
+
+  it(`String with letters repeating, case-sensetive, should return false`, () => {
+    expect(isIsogram('look'))
+      .toBe(false);
+    expect(isIsogram('loOk'))
+      .toBe(false);
   });
 });
