82f6746d8fe004a4a0f8343f029a8e91b2b20437
Anatolii Zinevych <anatolii.zinevych@gmail.com>
2024-02-20 21:10:47 +0200

    added tests

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,28 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  test(`should return true when input string
+   has uppercase and no repeatings `, () => {
+    expect(isIsogram('Kostia')).toBe(true);
+  });
+
+  test(`should return falce when input string
+   has uppercase and repeatings' `, () => {
+    expect(isIsogram('Marichka')).toBe(false);
+  });
+
+  test(`should return falce when input string
+   has uppercase and repeatings' `, () => {
+    expect(isIsogram('Kateryna')).toBe(false);
+  });
+
+  test(`should return true when input string
+   has uppercase and no repeatings' `, () => {
+    expect(isIsogram('Kate')).toBe(true);
+  });
+
+  test(`should return true when input string are empty `, () => {
+    expect(isIsogram('')).toBe(true);
+  });
 });
