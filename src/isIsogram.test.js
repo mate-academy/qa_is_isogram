06cfa6b9a6a4bf98c3090866ddf73053c4073551
Bogdan Stratiychuk <stratiychuk.bogdan@gmail.com>
2024-03-07 18:22:32 +0100

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,29 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`returns 'true' on 'epty string' input`, () => {
+    const result = isIsogram('');
+
+    expect(result).toBe(true);
+  });
+
+  it(`is case-insensitive`
+  + `, returns 'true' on 'Iryna' input`, () => {
+    const result = isIsogram('Iryna');
+
+    expect(result).toBe(true);
+  });
+
+  it(`returns 'false' on 'Door' input`, () => {
+    const result = isIsogram('Door');
+
+    expect(result).toBe(false);
+  });
+
+  it(`returns 'true' on 'playgrounds' input`, () => {
+    const result = isIsogram('playgrounds');
+
+    expect(result).toBe(true);
+  });
 });
