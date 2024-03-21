1555156c821abebc79fe0c8ca1c510c9c2828fcf
Katerina Poliit <alexispoliyit@gmail.com>
2024-03-21 11:01:15 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,17 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should work independently from Upper and Lower cases', () => {
+    expect(isIsogram('Monument')).toBe(true);
+  });
+
+  it('should return false when the word is not an isogram', () => {
+    expect(isIsogram('Kati')).toBe(false);
+  });
+
+  it('should return true for an empty argument', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
 });
