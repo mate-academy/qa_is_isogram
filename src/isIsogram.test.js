99eed255991052170228fc0ee85d9bf12351b9c1
ivanm2706 <ivanmorhun664@gmail.com>
2024-03-22 13:42:27 -0500

    added tests for function isIsogram

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,28 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return 'true' if word is ""isogram""`, () => {
+    const result = isIsogram('work');
+
+    expect(result).toBeTruthy();
+  });
+
+  it(`should return 'false' if word is not ""isogram""`, () => {
+    const result = isIsogram('sunset');
+
+    expect(result).toBeFalsy();
+  });
+
+  it('should non depend from cases', () => {
+    const result = isIsogram('immediatE');
+
+    expect(result).toBeFalsy();
+  });
+
+  it('should work with empty string', () => {
+    const result = isIsogram('');
+
+    expect(result).toBeTruthy();
+  });
 });
