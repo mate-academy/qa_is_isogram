90aec448138b92e1757425fbf9ab1801783fb5d3
Belia Anton <belja.anton@gmail.com>
2024-04-02 11:43:08 +0300

    add test to function isIsogram

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 'use strict';
 
 describe('isIsogram', () => {
@@ -5,5 +6,41 @@ describe('isIsogram', () => {
 
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
+  });
+
+  it('should return true for an empty string', () => {
+    const result = isIsogram('');
+
+    expect(result).toBe(true);
+  });
+
+  it('should return true for an isogram', () => {
+    const result = isIsogram('abcdefg');
+
+    expect(result).toBe(true);
+  });
+
+  it('should return false for a non-isogram with repeated letters', () => {
+    const result = isIsogram('hello');
+
+    expect(result).toBe(false);
+  });
+
+  it('should return true for a string with only one letter', () => {
+    const result = isIsogram('a');
+
+    expect(result).toBe(true);
+  });
+
+  it('should return false for a non-isogram with repeated letters, ignoring case', () => {
+    const result = isIsogram('HelLo');
+
+    expect(result).toBe(false);
+  });
+
+  it('should return false for a string with repeated characters and spaces', () => {
+    const result = isIsogram('the quick brown fox jumps over the lazy dog');
+
+    expect(result).toBe(false);
   });
 });
