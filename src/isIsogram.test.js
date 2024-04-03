d0d020e7fe29a076da2075474fc7d1a23522b7d4
Denys <sviastundenus@gmail.com>
2024-04-03 19:05:02 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -3,7 +3,29 @@
 describe('isIsogram', () => {
   const { isIsogram } = require('./isIsogram');
 
-  it(`should be declared`, () => {
-    expect(isIsogram).toBeInstanceOf(Function);
+  it('should be declared', () => {
+    expect(isIsogram)
+      .toBeInstanceOf(Function);
+  });
+
+  it('should return true for empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it('should return true for isograms', () => {
+    expect(isIsogram('Dermatoglyphics')).toBe(true);
+    expect(isIsogram('isogram')).toBe(true);
+    expect(isIsogram('subdermatoglyphic')).toBe(true);
+  });
+
+  it('should return false for words with repeating letters', () => {
+    expect(isIsogram('hello')).toBe(false);
+    expect(isIsogram('isIsogram')).toBe(false);
+    expect(isIsogram('aba')).toBe(false);
+  });
+
+  it('should be case insensitive', () => {
+    expect(isIsogram('Dermatoglyphics')).toBe(true);
+    expect(isIsogram('dermatoglyphics')).toBe(true);
   });
 });
