191e8ac2a08d552451628bfebc38823055762eaf
Agillka <mrmadness6661313@gmail.com>
2024-04-03 20:03:47 +0300

    Solutuon

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,40 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  test(`should return boolean`, () => {
+    const result = isIsogram('playground');
+
+    expect(typeof result).toBe('boolean');
+  });
+
+  test(`should return False when input is 'Mumspagetti'`, () => {
+    const result = isIsogram('Mumspagetti');
+
+    expect(result).toBeFalsy();
+  });
+
+  test(`should return True when input is 'sun'`, () => {
+    const result = isIsogram('sun');
+
+    expect(result).toBeTruthy();
+  });
+
+  test(`should return False when input is 'sunrise'`, () => {
+    const result = isIsogram('sunrise');
+
+    expect(result).toBeFalsy();
+  });
+
+  test(`should return True when input is ''`, () => {
+    const result = isIsogram('');
+
+    expect(result).toBeTruthy();
+  });
+
+  test(`should return True when input is ' '`, () => {
+    const result = isIsogram(' ');
+
+    expect(result).toBeTruthy();
+  });
 });
