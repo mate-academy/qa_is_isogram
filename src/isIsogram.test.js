623c93b9d4f095e9fae75065b1eb57094c862e08
Sandra <malinevskaqa@gmail.com>
2024-04-03 19:35:10 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -4,6 +4,38 @@ describe('isIsogram', () => {
   const { isIsogram } = require('./isIsogram');
 
   it(`should be declared`, () => {
-    expect(isIsogram).toBeInstanceOf(Function);
+    expect(isIsogram('playgrounds')).toEqual(true);
+  });
+});
+
+describe('isIsogram', () => {
+  const { isIsogram } = require('./isIsogram');
+
+  it(`should be declared`, () => {
+    expect(isIsogram('Playgrounds')).toEqual(true);
+  });
+});
+
+describe('isIsogram', () => {
+  const { isIsogram } = require('./isIsogram');
+
+  it(`should be declared`, () => {
+    expect(isIsogram('Adam')).toEqual(false);
+  });
+});
+
+describe('isIsogram', () => {
+  const { isIsogram } = require('./isIsogram');
+
+  it(`should be declared`, () => {
+    expect(isIsogram('look')).toEqual(false);
+  });
+});
+
+describe('isIsogram', () => {
+  const { isIsogram } = require('./isIsogram');
+
+  it(`should be declared`, () => {
+    expect(isIsogram(' ')).toEqual(true);
   });
 });
