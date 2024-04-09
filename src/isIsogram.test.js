377fd42601443eee83b62f4cc0e61e9014f44171
Your Name <your_email@whatever.com>
2024-04-09 16:22:56 +0300

    solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should return true for an empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it('should be case-insensitive', () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it('should return true for the word "playgrounds"', () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it('should return false for the word "look"', () => {
+    expect(isIsogram('look')).toBe(false);
+  });
 });
