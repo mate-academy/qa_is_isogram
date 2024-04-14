995557d3aa5e313e654278bec17971e7c9490919
TykhonKozachenko <tooliewae@gmail.com>
2024-04-14 19:00:14 +0200

    tests

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -8,13 +8,28 @@ describe('isIsogram', () => {
       .toBeInstanceOf(Function);
   });
 
+  it('should return a boolean value', () => {
+    expect(typeof isIsogram('playground'))
+      .toBe('boolean');
+  });
+
   it('should return true for an empty string', () => {
     expect(isIsogram(''))
       .toBe(true);
   });
 
+  it('should return true for a string with only one letter', () => {
+    expect(isIsogram('a'))
+      .toBe(true);
+  }); // technically one-letter string could be considered as an isogram
+
   it('should return true for a word without repeating letters', () => {
     expect(isIsogram('playground'))
+      .toBe(true);
+  });
+
+  it('should return true for a word from different languages', () => {
+    expect(isIsogram('привіт'))
       .toBe(true);
   });
 
