e13615c8cfcc81bb1bc7ae597592cb7a2c88b5d9
TykhonKozachenko <tooliewae@gmail.com>
2024-04-14 11:03:08 +0200

    tests

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -3,7 +3,28 @@
 describe('isIsogram', () => {
   const { isIsogram } = require('./isIsogram');
 
-  it(`should be declared`, () => {
-    expect(isIsogram).toBeInstanceOf(Function);
+  it(`function should be declared`, () => {
+    expect(isIsogram)
+      .toBeInstanceOf(Function);
+  });
+
+  it('should return true for an empty string', () => {
+    expect(isIsogram(''))
+      .toBe(true);
+  });
+
+  it('should return true for a word without repeating letters', () => {
+    expect(isIsogram('playground'))
+      .toBe(true);
+  });
+
+  it('should return false for a word with repeating letters', () => {
+    expect(isIsogram('look'))
+      .toBe(false);
+  });
+
+  it('should be case-insensitive', () => {
+    expect(isIsogram('Adam'))
+      .toBe(false);
   });
 });
