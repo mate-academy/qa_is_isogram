b7f47233a1c21ddaed303f2921d48413805712a6
Rozdorozhnii <rozdorozhnii@ukr.net>
2024-04-18 23:00:41 +0300

    Solution_v1.0

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,34 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should return boolean', () => {
+    const result = isIsogram('abc');
+
+    expect(typeof result === 'boolean').toBeTruthy();
+  });
+
+  it('should return true for empty string', () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
+
+  it('should return false for non-consecutive duplicate', () => {
+    expect(isIsogram('test')).toBeFalsy();
+  });
+
+  it('should return false for capitalized duplicate', () => {
+    expect(isIsogram('Adam')).toBeFalsy();
+  });
+
+  it('should return false for consecutive duplicate', () => {
+    expect(isIsogram('look')).toBeFalsy();
+  });
+
+  it('should return false for triplet and duplicate', () => {
+    expect(isIsogram('bananas')).toBeFalsy();
+  });
+
+  it('should return true for \'isogram\'', () => {
+    expect(isIsogram('playgrounds')).toBeTruthy();
+  });
 });
