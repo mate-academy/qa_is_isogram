184c67ebbdf1496180f5f92ffd75c127bfc100d2
Yehor-f2006 <151622806+Yehor-f2006@users.noreply.github.com>
2024-05-08 20:02:34 +0300

    solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,22 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('no repeating letters', () => {
+    const result = isIsogram('wonderful');
+
+    expect(result).toEqual(true);
+  });
+
+  it('is case insensitive', () => {
+    const result = isIsogram('CoOl');
+
+    expect(result).toEqual(false);
+  });
+
+  it('returns true for an empty argument', () => {
+    const result = isIsogram('');
+
+    expect(result).toEqual(true);
+  });
 });
