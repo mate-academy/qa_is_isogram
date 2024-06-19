949573285a33d088d0c04658117aa90927c42cbc
Dmytro Manankin <dmitry.manankin@gmail.com>
2024-06-19 19:53:33 +0300

    task complete

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,32 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  test('should return TRUE for empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  test('should return TRUE for the string of 1 char', () => {
+    expect(isIsogram('a')).toBe(true);
+  });
+
+  test('should return FALSE for the string of two similar chars', () => {
+    expect(isIsogram('bb')).toBe(false);
+  });
+
+  test('should return TRUE for isogram string same-case', () => {
+    expect(isIsogram('playwords')).toBe(true);
+  });
+
+  test('should return FALSE for non-isogram string same-case', () => {
+    expect(isIsogram('playworlds')).toBe(false);
+  });
+
+  test('should return FALSE for non-isogram string dif-case', () => {
+    expect(isIsogram('helLo')).toBe(false);
+  });
+
+  test('should return TRUE for non-isogram string dif-case', () => {
+    expect(isIsogram('WelCominG')).toBe(true);
+  });
 });
