69e63b1e9fceb04281097f5cecc221cc4eb91aee
Bohdan98Kuch <bohdan13kuch@gmail.com>
2024-06-21 18:27:57 +0300

    sollution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -1,9 +1,27 @@
 'use strict';
 
 describe('isIsogram', () => {
+
   const { isIsogram } = require('./isIsogram');
 
-  it(`should be declared`, () => {
-    expect(isIsogram).toBeInstanceOf(Function);
+
+    test('returns true for an empty string', () => {
+      expect(isIsogram('')).toBe(true);
+    });
+  
+    test('returns true for the word "playgrounds"', () => {
+      expect(isIsogram('playgrounds')).toBe(true);
+    });
+  
+    test('returns false for the word "look"', () => {
+      expect(isIsogram('look')).toBe(false);
+    });
+  
+    test('returns false for the word "Adam"', () => {
+      expect(isIsogram('Adam')).toBe(false);
+    });
+  
+    test('handles words with repeating characters', () => {
+      expect(isIsogram('bookkeeper')).toBe(false);
+    });
   });
-});
