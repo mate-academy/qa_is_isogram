de225ff88839e658936fab96f382d43985cac3f1
Oleg Voloshyn <voloshyn.oleg.oleg@gmail.com>
2024-06-21 17:12:51 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,26 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return false when the 'word' contains numbers`, () => {
+    expect(isIsogram('mate2024')).toBe(false);
+  });
+
+  it(`should return false if the 'word' contains special characters`, () => {
+    expect(isIsogram('mate_2024')).toBe(false);
+  });
+
+  it(`should return false when the letters in the 'word' are repeated`, () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it(`should return true when the letters in the
+    'word' are not repeated`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it(`should return true when the letters in the
+    'word' contains an uppercase letter`, () => {
+    expect(isIsogram('Dermatoglyphics')).toBe(true);
+  });
 });
