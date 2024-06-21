9c2c0049f02d5b2d7bccb50adcccb006311515c5
Khrystyna Kazva <kristinakazva@gmail.com>
2024-06-21 19:26:57 +0300

    Add test for isIsogram function

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  test(`should return true for an empty string`, () => {
+    expect(isIsogram('')).toEqual(true);
+  });
+
+  test(`should return false for a repeating letters string`, () => {
+    expect(isIsogram('look')).toEqual(false);
+  });
+
+  test(`should return true for a non-repeating letters string`, () => {
+    expect(isIsogram('playgrounds')).toEqual(true);
+  });
+
+  test(`should return false for a mixed case repeating letters string`, () => {
+    expect(isIsogram('loOk')).toEqual(false);
+  });
 });
