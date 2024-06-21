d4ca5a208b011fefa647e855239a3aa2ad156a54
yuliayahusta <yuliayahusta@gmail.com>
2024-06-21 21:31:18 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,34 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should be returned true for ''`, () => {
+    expect(isIsogram('playground'))
+      .toBe(true);
+  });
+
+  it(`should be returned true for 'MATE'`, () => {
+    expect(isIsogram('MATE'))
+      .toBe(true);
+  });
+
+  it(`should be returned true for 'QAmentor'`, () => {
+    expect(isIsogram('QAmentor'))
+      .toBe(true);
+  });
+
+  it(`should be returned false for 'isolation'`, () => {
+    expect(isIsogram('isolation'))
+      .toBe(false);
+  });
+
+  it(`should be returned false for 'Obviously'`, () => {
+    expect(isIsogram('Obviously'))
+      .toBe(false);
+  });
+
+  it(`should be returned false for 'aaaaa'`, () => {
+    expect(isIsogram('aaaaa'))
+      .toBe(false);
+  });
 });
