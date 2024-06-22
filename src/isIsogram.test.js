473a6c592eea4688c5c1439ece0c9f26424c93df
Roman Gamza <romangamza@gmail.com>
2024-06-22 19:41:54 +0300

    soooo solved)

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,27 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true if value is empty`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`String that has no repeating letters,
+    case-insensetive, should return true`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it(`String with letters repeating, case-sensetive,
+    should return false`, () => {
+    expect(isIsogram('look')).toBe(false);
+    expect(isIsogram('loOk')).toBe(false);
+});
+
+  it(`Non valid input should return false`, () => {
+    expect(isIsogram(null)).toBe(false);
+
+    expect(isIsogram(987654)).toBe(false);
+
+    expect(isIsogram(-133)).toBe(false);
+  });
 });
