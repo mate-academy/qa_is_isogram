17e8830807ec7060de53466d97f42dbaf908d600
InnaKurtieva <nezaleznaua2308@ukr.net>
2024-06-23 15:52:30 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true if isIsogram('')`, () => {
+    expect(isIsogram === true);
+  });
+
+  it(`should return true if isIsogram includes different letters`, () => {
+    expect(isIsogram === true);
+  });
+
+  it(`should return true if isIsogram has letters in divers registers`, () => {
+    expect(isIsogram === true);
+  });
+
+  it(`should return false if isIsogram includes identical letters`, () => {
+    expect(isIsogram === false);
+  });
 });
