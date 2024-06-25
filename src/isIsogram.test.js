f77c9675985c88c3fc564c756d6a0a9c891747f6
yurayagu <yurayagu@gmail.com>
2024-06-25 14:01:49 +0300

    Improved tests

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -7,39 +7,39 @@ describe('isIsogram', () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
 
-  it('should return true for an empty string', () => {
+  it('should return true if entered empty string has no repeatings', () => {
     expect(isIsogram('')).toBe(true);
   });
 
-  it('should return true for "playgrounds"', () => {
-    expect(isIsogram('playgrounds')).toBe(true);
+  it('should return true if entered string has no repeatings', () => {
+    expect(isIsogram('y')).toBe(true);
   });
 
-  it('should return false for "look"', () => {
-    expect(isIsogram('look')).toBe(false);
+  it('should return true if entered string has no repeatings', () => {
+    expect(isIsogram('Y')).toBe(true);
   });
 
-  it('should return false for "Adam"', () => {
+  it('should return false if entered string has repeatings', () => {
+    expect(isIsogram('yY')).toBe(false);
+  });
+
+  it('should return true if entered string has no repeatings', () => {
+    expect(isIsogram('Yura')).toBe(true);
+  });
+
+  it('should return false if entered string has repeatings', () => {
     expect(isIsogram('Adam')).toBe(false);
   });
 
-  it('should return true for "isogram"', () => {
-    expect(isIsogram('isogram')).toBe(true);
+  it('should return false if entered string has repeatings', () => {
+    expect(isIsogram('Yurii')).toBe(false);
   });
 
-  it('should return true for "qa"', () => {
-    expect(isIsogram('qa')).toBe(true);
+  it('should return true if entered string has no repeatings', () => {
+    expect(isIsogram('abcdefghijklmnopqrstuvwxyz')).toBe(true);
   });
 
-  it('should return true for "may"', () => {
-    expect(isIsogram('may')).toBe(true);
-  });
-
-  it('should return true for "yura"', () => {
-    expect(isIsogram('yura')).toBe(true);
-  });
-
-  it('should return false for "yahusta"', () => {
-    expect(isIsogram('yahusta')).toBe(false);
+  it('should return false if entered string has repeatings', () => {
+    expect(isIsogram('Aabcdefghijklmnopqrstuvwxyz')).toBe(false);
   });
 });
