cb909606083e99539d28e258a21b2899ad0c5e8f
Artem Zakharchuk <artemzakharchuk@gmail.com>
2024-07-08 16:20:30 +0300

    solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,28 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('works with empty line', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it('works with one letter', () => {
+    expect(isIsogram('d')).toBe(true);
+  });
+
+  it('return "false" if input="Dad"', () => {
+    expect(isIsogram('Dad')).toBe(false);
+  });
+
+  it('return "true" if different case', () => {
+    expect(isIsogram('qWeRtY')).toBe(true);
+  });
+
+  it('return "true" if input="uncopyrightable"', () => {
+    expect(isIsogram('uncopyrightable')).toBe(true);
+  });
+
+  it('return "false" if input="uncopyrightable u"', () => {
+    expect(isIsogram('uncopyrightable u')).toBe(false);
+  });
 });
