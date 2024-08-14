1806cbf54cb88c4d31e7c9f86a0e7102dfa9d11e
boytrend0108 <boytrend@gmail.com>
2024-08-14 16:38:11 +0300

    complete

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,16 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should be declared`, () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it(`should be declared`, () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it(`should be declared`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
 });
