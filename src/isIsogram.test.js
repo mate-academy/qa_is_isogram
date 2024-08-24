abb2123ae48b686ae3745be0ab1c6c28d250a7f4
Eduard Herasym <edkido29@gmail.com>
2024-08-24 18:48:19 +0300

    tests

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,27 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true or false`, () => {
+    const result = isIsogram('word');
+    const value = toString.call(result);
+
+    expect(value).toBe('[object Boolean]');
+  });
+
+  it(`should be case insensitive`, () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it(`should return true for empty 'word'`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`should return true for 'word' that has no repeating letters`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it(`should return false for 'word' that has repeating letters`, () => {
+    expect(isIsogram('playgrounds look')).toBe(false);
+  });
 });
