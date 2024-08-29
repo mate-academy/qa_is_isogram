9631b7e760bd6ab7e182f99da54912dd0c172c6c
RomanOstrous <ostrous.roman@gmail.com>
2024-08-30 00:30:05 +0300

    add task solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,21 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should be retourn 'true' if word is isogram`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it(`should be retourn 'false' if word include duplicate letters`, () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it(`should be retourn 'false' if
+    word include duplicate capital letters`, () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it(`should be retourn 'true' if an empty string is passed`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
 });
