b60e3c35fef5748283f3c1c39ec2e258a218b87c
maksim-atanasov <atanasovmaksim56@gmail.com>
2024-08-31 15:12:37 +0300

    solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return 'true', if 'playgrounds'`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it(`should return 'false', if 'look'`, () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it(`should return 'false', if 'Adam'`, () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it(`should return 'true', if empty string`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
 });
