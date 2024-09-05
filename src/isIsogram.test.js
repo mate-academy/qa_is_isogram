0323079309774c5300c3e6be6dc931a1cf6a746b
Svitlana <depakinechrono@ukr.net>
2024-09-05 18:53:47 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,25 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should be true if word is empty`, () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
+
+  it(`should be true
+      if word contains non-repeating letters`, () => {
+    expect(isIsogram('Exam')).toBeTruthy();
+  });
+
+  it(`should be false
+      if word contains repeating letters
+        in different cases`, () => {
+    expect(isIsogram('Ilike')).toBeFalsy();
+  });
+
+  it(`should be false
+      if word contains repeating letters
+        in the same case`, () => {
+    expect(isIsogram('ilike')).toBeFalsy();
+  });
 });
