599206989576f6a4653159fecb01f714a9e0bfd0
lemeshev <mate.lemeshev@gmail.com>
2024-09-05 14:24:13 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -1,9 +1,26 @@
 'use strict';
 
 describe('isIsogram', () => {
-  const { isIsogram } = require('./isIsogram');
+const { isIsogram } = require('./isIsogram');
 
-  it(`should be declared`, () => {
-    expect(isIsogram).toBeInstanceOf(Function);
-  });
+it(`should be declared`, () => {
+expect(isIsogram).toBeInstanceOf(Function);
+});
+
+it(`should return true when string contain word that has no repeating letters`, () => {
+expect(isIsogram('youare')).toBe(true);
+});
+
+it(`should return false when string contain word that has no repeating consecutive letters`, () => {
+expect(isIsogram('hello')).toBe(false);
+});
+
+it(`should return false when string contain word that has no repeating non-consecutive letters`, () => {
+expect(isIsogram('couch')).toBe(false);
+});
+
+it(`should return true when string is empty`, () => {
+expect(isIsogram('')).toBe(true);
+});
+
 });
