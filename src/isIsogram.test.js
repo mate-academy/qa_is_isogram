7d7c595b559b2b79ebf81beb5039eb03b592bc11
Vita <st.vitalina666@gmail.com>
2024-09-05 15:38:23 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,30 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('shoud return true for empty string', () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
+
+  it(`shoud return true if a word has no repeating letters`, () => {
+    expect(isIsogram('lemon')).toBeTruthy();
+  });
+
+  it(`shoud return false if a word has
+    consecutive repeating letters`, () => {
+    expect(isIsogram('Apple')).toBeFalsy();
+  });
+
+  it(`shoud return false if a word has
+    non-consecutive repeating letters`, () => {
+    expect(isIsogram('Dragarys')).toBeFalsy();
+  });
+
+  it('shoud return true for empty string', () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
+
+  it('shoud be case insensetive', () => {
+    expect(isIsogram('Emet')).toBeFalsy();
+  });
 });
