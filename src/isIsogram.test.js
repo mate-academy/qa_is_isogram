785564587cee9de040192629bc90ce5f0109cc00
Sofi <sofiiamamzer06@gmail.com>
2024-09-05 15:54:10 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,30 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('Should return false if word contains same letters', () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it(`Should return false
+    if word contains same letters with diff register`, () => {
+    expect(isIsogram('Dude')).toBe(false);
+  });
+
+  it('Should return true if word does not contains same letters', () => {
+    expect(isIsogram('play')).toBe(true);
+  });
+
+  it('Should return true if the string is empty', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`Should return true if word contains mixed casing
+    and unique letters`, () => {
+    expect(isIsogram('Ground')).toBe(true);
+  });
+
+  it('Should return true for single letter', () => {
+    expect(isIsogram('a')).toBe(true);
+  });
 });
