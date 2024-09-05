80c4cfe55c444d9a84d7bf19ade49d7abf403614
Oksana Tereshchuk <oxanatereschuk@gmail.com>
2024-09-05 18:36:04 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,28 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should return true for repeating letters', () => {
+    expect(isIsogram('isogram')).toBe(true);
+  });
+
+  it('should return true for an empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it('should return true for no repeating letters and case insensitive', () => {
+    expect(isIsogram('SERhiy')).toBe(true);
+  });
+
+  it('should return true for single-letter word "A"', () => {
+    expect(isIsogram('A')).toBe(true);
+  });
+
+  it('should return false for repeating character', () => {
+    expect(isIsogram('hello')).toBe(false);
+  });
+
+  it('should return false for repeating character and case insensitive', () => {
+    expect(isIsogram('OKSANA')).toBe(false);
+  });
 });
