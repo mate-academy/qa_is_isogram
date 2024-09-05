82c64d5e19582569fa045eb72f2d1831f4e4c20a
nevermore <gritsanowo@gmail.com>
2024-09-05 12:05:29 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -7,35 +7,35 @@ describe("isIsogram", () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
 
-  it("should true for an empty string", () => {
+  it("should return true for an empty string", () => {
     expect(isIsogram("")).toBe(true);
   });
 
-  it("should true for a single letter string", () => {
+  it("should return true for a single letter string", () => {
     expect(isIsogram("a")).toBe(true);
   });
 
-  it("should false with duplicate letters", () => {
+  it("should return false with duplicate letters", () => {
     expect(isIsogram("aab")).toBe(false);
   });
 
-  it("should true with all unique letters", () => {
+  it("should return true with all unique letters", () => {
     expect(isIsogram("abcde")).toBe(true);
   });
 
-  it("should true with mixed case letters", () => {
+  it("should return true with mixed case letters", () => {
     expect(isIsogram("Ab")).toBe(true);
   });
 
-  it("should true with non-alphabetic characters", () => {
+  it("should return true with non-alphabetic characters", () => {
     expect(isIsogram("a1b2c3")).toBe(true);
   });
 
-  it("should false with a repeated non-alphabetic chars", () => {
+  it("should return false with a repeated non-alphabetic characters", () => {
     expect(isIsogram("a1b2c3a")).toBe(false);
   });
 
-  it("should false with non-alphabetic charas in mixed case", () => {
+  it("should return false with non-alphabetic characters in mixed case", () => {
     expect(isIsogram("A1b2c3A")).toBe(false);
   });
 });
