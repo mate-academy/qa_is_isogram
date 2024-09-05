729f5ee2d98656608944311ca3343f05196105ca
Tetiana Perepelytsia <t.perepelytsya@gmail.com>
2024-09-05 17:26:49 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,25 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true for a word with no repeating letters`, () => {
+    expect(isIsogram('tower')).toBe(true);
+  });
+
+  it(`should return false for a word with repeating letters`, () => {
+    expect(isIsogram('letter')).toBe(false);
+  });
+
+  it(`should return true for an empty string`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`should return true for a word with
+     no repeating letters with mixed case`, () => {
+    expect(isIsogram('Camel')).toBe(true);
+  });
+
+  it(`should return true for a single letter`, () => {
+    expect(isIsogram('a')).toBe(true);
+  });
 });
