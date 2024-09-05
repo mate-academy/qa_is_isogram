827dd6904fc9c4a42530d051a2383e0f78a3550d
Maxim Berestovyi <maxl.beresto2019@gmail.com>
2024-09-05 18:10:27 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -3,7 +3,25 @@
 describe('isIsogram', () => {
   const { isIsogram } = require('./isIsogram');
 
-  it(`should be declared`, () => {
-    expect(isIsogram).toBeInstanceOf(Function);
+  test('should return true for an empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  test('should return true for a string with all unique letters', () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  test('should return false for a string with repeated letters', () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  test(`should return false for a string with
+    letters differing only by case`, () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  test('should return true for a string with only one letter', () => {
+    expect(isIsogram('a')).toBe(true);
+    expect(isIsogram('Z')).toBe(true);
   });
 });
