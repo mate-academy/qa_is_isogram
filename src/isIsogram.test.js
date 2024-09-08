daf24fbe2fc4edbac2266a6e40ea5ba5cf5cc315
Rodionsav <rodionsav@mate.academy>
2024-09-08 17:40:46 +0200

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -1,31 +1,31 @@
-"use strict";
+'use strict';
 
-describe("isIsogram", () => {
-  const { isIsogram } = require("./isIsogram");
+describe('isIsogram', () => {
+  const { isIsogram } = require('./isIsogram');
 
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
 
   it(`should be true if word is empty`, () => {
-    expect(isIsogram).toBeTruthy();
+    expect(isIsogram('')).toBeTruthy();
   });
 
   it(`should be true if word contain non-repeating letters`, () => {
-    expect(isIsogram).toBeTruthy();
+    expect(isIsogram('playgrounds')).toBeTruthy();
   });
 
   it('should return false for "look" (not an isogram)', () => {
-    expect(isIsogram("look")).toBeFalsy();
+    expect(isIsogram('look')).toBeFalsy();
   });
 
   it('should return false for "Adam" (case-insensitive)', () => {
-    expect(isIsogram("Adam")).toBeFalsy();
+    expect(isIsogram('Adam')).toBeFalsy();
   });
 
   it(`should be false
     if word contains repeating letters
       in the same case`, () => {
-    expect(isIsogram("adam")).toBeFalsy();
+    expect(isIsogram('adam')).toBeFalsy();
   });
 });
