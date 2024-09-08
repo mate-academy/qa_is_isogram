85ec0889728d40fd7dbe153b2764693d651d4122
Mariana Khrustina <khrystinamariana@gmail.com>
2024-09-09 02:00:19 +0400

    Added isIsogram function and tests

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -4,32 +4,32 @@ const { isIsogram } = require('./isIsogram');
 
 describe('isIsogram', () => {
   it(
-    'should return true for an empty string because an empty string ' +
-    'has no repeating letters',
+    'should return true for an empty string because an empty string '
+    + 'has no repeating letters',
     () => {
       expect(isIsogram('')).toBe(true);
     }
   );
 
   it(
-    'should return true for an isogram string like "playgrounds" ' +
-    'because all letters are unique',
+    'should return true for an isogram string like "playgrounds" '
+    + 'because all letters are unique',
     () => {
       expect(isIsogram('playgrounds')).toBe(true);
     }
   );
 
   it(
-    'should return false for a non-isogram string like "look" ' +
-    'because the letter "o" is repeated',
+    'should return false for a non-isogram string like "look" '
+    + 'because the letter "o" is repeated',
     () => {
       expect(isIsogram('look')).toBe(false);
     }
   );
 
   it(
-    'should return false for a non-isogram string like "Adam" ' +
-    'because the letter "a" is repeated (case-insensitive)',
+    'should return false for a non-isogram string like "Adam" '
+    + 'because the letter "a" is repeated (case-insensitive)',
     () => {
       expect(isIsogram('Adam')).toBe(false);
     }
