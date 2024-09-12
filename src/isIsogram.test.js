8e19b8defe05c4e6c700405174c81a065d5ffa86
Yaroslav Nazarenko <yinazarenkoo@gmail.com>
2024-09-12 15:54:31 +0200

    solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,30 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return 'true' if 'word' is empty`, () => {
+    const result = isIsogram('');
+
+    expect(result).toBe(true);
+  });
+
+  it(`should return 'true'
+    if 'word' does not contain any duplicate letters`, () => {
+    const result = isIsogram('abcde');
+
+    expect(result).toBe(true);
+  });
+
+  it(`should return 'false'
+    if 'word' contains duplicate letters`, () => {
+    const result = isIsogram('apple');
+
+    expect(result).toBe(false);
+  });
+
+  it(`should be case-insensitive`, () => {
+    const result = isIsogram('Adam');
+
+    expect(result).toBe(false);
+  });
 });
