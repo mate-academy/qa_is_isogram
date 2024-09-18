759d793816ae3cd0efb7c471fabad9f4813b0acd
luckezQA <labchuk2007@gmail.com>
2024-09-18 22:54:53 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -3,7 +3,28 @@
 describe('isIsogram', () => {
   const { isIsogram } = require('./isIsogram');
 
-  it(`should be declared`, () => {
+  it('should be declared', () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should return true for an empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it('should return true for "playgrounds"', () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it('should return false for "look"', () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it('should return false for "Adam"', () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it('should return true for "Machine"', () => {
+    expect(isIsogram('Machine')).toBe(true);
+  });
 });
+;
