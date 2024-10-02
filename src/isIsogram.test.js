2e1e400e975679a7f9f4488fabcd2d7bd72fea70
poznianski <poznianski@gmail.com>
2024-10-02 23:37:43 +0300

    sol

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it('no repeating letters', () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it('with repeating letters', () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it('case-insensitive', () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
 });
