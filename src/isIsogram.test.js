3cb5b3ed6e4041af55e63b0b79984c9d94ee8fb6
Valeriia Oliinyk <valeriia.oliinyk.dev@gmail.com>
2024-10-18 17:50:51 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,28 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should throw an error if 'word' includes not only letters`, () => {
+    expect(() => isIsogram('ght5re')).toThrow();
+  });
+
+  it(`should return 'true' if 'word' is empty string`, () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
+
+  it(`should return 'true' if 'word' = playgrounds`, () => {
+    expect(isIsogram('playgrounds')).toBeTruthy();
+  });
+
+  it(`should return 'false' if 'word' = look`, () => {
+    expect(isIsogram('look')).toBeFalsy();
+  });
+
+  it(`should return 'false' if 'word' = Adam`, () => {
+    expect(isIsogram('Adam')).toBeFalsy();
+  });
+
+  it(`should return 'false' if 'word' = Oops`, () => {
+    expect(isIsogram('Oops')).toBeFalsy();
+  });
 });
