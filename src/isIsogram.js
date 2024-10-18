ff37fd82b601c4f8cf5696ca8b94b7e6c1daf35b
SrTrace <levashov04sergey@gmail.com>
2024-10-18 16:22:54 +0300

    solution

--- src/isIsogram.js
+++ src/isIsogram.js
@@ -5,7 +5,12 @@
  *
  * @returns {boolean}
  */
+
 function isIsogram(word) {
+  if (typeof word !== 'string') {
+    return false;
+  }
+  
   const wordToLower = word.toLowerCase();
 
   for (let i = 0; i < wordToLower.length; i++) {
