8d3b3d11c993cc23ebd628e871ae7668b7fff4d4
Vladislav <njiuyfqqq123@gmail.com>
2024-10-28 18:33:59 +0200

    done

--- src/isIsogram.js
+++ src/isIsogram.js
@@ -1,4 +1,5 @@
-'use strict';
+/* eslint quotes: ["off"] */
+"use strict";
 
 /**
  * @param {string} word
--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -1,9 +1,46 @@
-'use strict';
+/* eslint quotes: ["off"] */
+"use strict";
 
-describe('isIsogram', () => {
-  const { isIsogram } = require('./isIsogram');
+describe("isIsogram", () => {
+  const { isIsogram } = require("./isIsogram");
 
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
+  });
+
+  it("should return a boolean value", () => {
+    const result = isIsogram("");
+
+    expect(typeof result).toBe("boolean");
+  });
+
+  it("should return true for a valid isogram", () => {
+    const result = isIsogram("playgrounds");
+
+    expect(result).toBeTruthy();
+  });
+
+  it("should return false for a word with repeated letters", () => {
+    const result = isIsogram("look");
+
+    expect(result).toBeFalsy();
+  });
+
+  it("should be case insensitive", () => {
+    const result = isIsogram("Adam");
+
+    expect(result).toBeFalsy();
+  });
+
+  it("should return true for an empty string", () => {
+    const result = isIsogram("");
+
+    expect(result).toBeTruthy();
+  });
+
+  it("should return true for single-letter words", () => {
+    const result = isIsogram("A");
+
+    expect(result).toBeTruthy();
   });
 });
