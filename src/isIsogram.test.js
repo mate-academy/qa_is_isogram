7e49260e7fdb060948a12eeeb002588adf93425d
Pavlo Petrashevskyi <dragonthunderaddsdp@gmail.com>
2024-11-02 16:50:48 +0200

    tests for isIsogram were created

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,24 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should throw an error if a 'word' is not a string`, () => {
+    expect(() => isIsogram(NaN)).toThrow();
+  });
+
+  it('should handle empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it('should return true if a word is isogram', () => {
+    expect(isIsogram('question')).toBe(true);
+  });
+
+  it('should return false if a word is not isogram', () => {
+    expect(isIsogram('mafia')).toBe(false);
+  });
+
+  it('should be case insensitive', () => {
+    expect(isIsogram('MammA')).toBe(false);
+  });
 });
