3ab4b019b92e9e4a4d92801c1bc1bd09d8c31fbb
Oleksandr <dobivalit@gmail.com>
2024-11-06 20:22:41 +0200

    Solution

--- src/isIsogram.js
+++ src/isIsogram.js
@@ -6,10 +6,24 @@
  * @returns {boolean}
  */
 function isIsogram(word) {
+  // Given the requirements of the task, based on the TDD method,
+  // I supplemented the function with checks for input values.
+
+  // Checking that "word" is a string
+  if (typeof (word) !== 'string' && word !== '') {
+    return false;
+  }
+
+  const lettersString = 'qwertyuiopasdfghjklzxcvbnm';
   const wordToLower = word.toLowerCase();
 
   for (let i = 0; i < wordToLower.length; i++) {
     const letter = wordToLower[i];
+
+    // Checking that "word" contains only letters
+    if (!lettersString.includes(letter)) {
+      return false;
+    }
 
     if (wordToLower.includes(letter, i + 1)) {
       return false;
--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -2,8 +2,49 @@
 
 describe('isIsogram', () => {
   const { isIsogram } = require('./isIsogram');
+  const stringExample = NaN;
 
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
+  });
+
+  it(`should return 'false' if 'word' is not a string`, () => {
+    const result = isIsogram(stringExample);
+
+    if (typeof (stringExample) !== 'string' && stringExample !== '') {
+      expect(result).toBe(false);
+    }
+  });
+
+  it('should ignore case-insensitive', () => {
+    const result = isIsogram('Aabc');
+
+    expect(result).toBe(false);
+  });
+
+  it(`should return 'false' if 'word' contains non-letters`, () => {
+    const result = isIsogram(stringExample);
+
+    if (typeof (stringExample) === 'string') {
+      const arrayFromString = Array.from(stringExample);
+
+      expect(result).toBe(arrayFromString.every((value) => {
+        const lettersString = 'qwertyuiopasdfghjklzxcvbnm1';
+
+        lettersString.includes(value.toLowerCase());
+      }));
+    };
+  });
+
+  it(`should return "true" if the string "word" is ('')`, () => {
+    const result = isIsogram('');
+
+    expect(result).toBe(true);
+  });
+
+  it('should return "false" if the string "word" is not an isogram', () => {
+    const result = isIsogram('AbcD');
+
+    expect(result).toBe(true);
   });
 });
