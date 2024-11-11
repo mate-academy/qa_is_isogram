1c115a584d7d951d752773550f6023fd9e12a14b
Kate Marchuk <pion3219@gmail.com>
2024-11-11 11:52:18 +0200

    Added solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,24 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true for an empty string`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`should return true for a word without repeated letters`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it(`should return true for a word with uppercase letters and without repeated letters`, () => {
+    expect(isIsogram('BeAr')).toBe(true);
+  });
+
+  it(`should return false for a word with repeated letters`, () => {
+    expect(isIsogram('macaron')).toBe(false);
+  });
+
+  it(`should return false for a word with uppercase and repeated letters`, () => {
+    expect(isIsogram('Oops')).toBe(false);
+  });
 });
