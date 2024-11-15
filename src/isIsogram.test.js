dafcd9492752842a12a27ad7b80e9f548ce57077
Bulavskyi Oleh <bulavskyi.oleh@gmail.com>
2024-11-15 21:47:26 +0200

    Unit test

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,33 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`true for an empty string`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`true for a single letter`, () => {
+    expect(isIsogram('a')).toBe(true);
+  });
+
+  it(`is case-insensitive`, () => {
+    expect(isIsogram('Ccart')).toBe(false);
+  });
+
+  it(`does not accept numbers`, () => {
+    expect(isIsogram('My Oleh2')).toBe(false);
+  });
+
+  it(`ignores spaces`, () => {
+    expect(isIsogram('   oleh')).toBe(true);
+  });
+
+  it('positive case', () => {
+    expect(isIsogram('abcdefghij')).toBe(true);
+  });
+
+  it('accepts non-latin letters', () => {
+    expect(isIsogram('борна')).toBe(true);
+  });пше 
+
 });
