7d74ec64c329297ad4305aeb0bf34c4b8484be78
Clyacsa <clyacsa@gmail.com>
2024-11-16 01:32:43 +0200

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,24 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`enters izogram`, () => {
+    expect(isIsogram('horse')).toBe(true);
+  });
+
+  it(`enters not izogram`, () => {
+    expect(isIsogram('mathematic')).toBe(false);
+  });
+
+  it(`enters with Uppercase not izogram`, () => {
+    expect(isIsogram('Clock')).toBe(false);
+  });
+
+  it(`enters with Uppercase izogram`, () => {
+    expect(isIsogram('Term')).toBe(true);
+  });
+
+  it(`enters with Uppercase izogram`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
 });
