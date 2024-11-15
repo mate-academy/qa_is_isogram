8b01def410a7b785fcc27c72ce7ebd714eb119e1
Nariman Mambetov <narik.mamb1@gmail.com>
2024-11-15 20:52:02 +0200

    UpIslgram

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,29 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true for a word with all unique letters`, () => {
+    expect(isIsogram('playgrounds'))
+      .toBe(true);
+  });
+
+  it(`should return false for a word with repeating letters`, () => {
+    expect(isIsogram('look'))
+      .toBe(false);
+  });
+
+  it(`should return false for a word with case-insensitive duplicates`, () => {
+    expect(isIsogram('Adam'))
+      .toBe(false);
+  });
+
+  it(`should return true for an empty string`, () => {
+    expect(isIsogram(''))
+      .toBe(true);
+  });
+
+  it(`should return false for a word with repeating letters`, () => {
+    expect(isIsogram('Oops'))
+      .toBe(false);
+  });
 });
