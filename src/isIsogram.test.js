78747779a8f9b09e9a05e889ad7ffb402a27a09a
Vlad Orlovskyy <vladorlovskyy@gmail.com>
2024-11-15 15:43:54 +0200

    Add changes

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -3,7 +3,35 @@
 describe('isIsogram', () => {
   const { isIsogram } = require('./isIsogram');
 
-  it(`should be declared`, () => {
+  it('should be declared', () => {
     expect(isIsogram).toBeInstanceOf(Function);
+  });
+
+  it('should return true for a word with no repeating letters', () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it('should return false for a word with repeating letters', () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it('should return false for a word with repeating letters', () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it('should return true for an empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it('should return false for a word with repeating letters', () => {
+    expect(isIsogram('Oops')).toBe(false);
+  });
+
+  it('should return true for a word with unique letters', () => {
+    expect(isIsogram('abcdef')).toBe(true);
+  });
+
+  it('should return false for a word with a single repeating letter', () => {
+    expect(isIsogram('letter')).toBe(false);
   });
 });
