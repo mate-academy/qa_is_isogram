a46335ceb090c6cffcb785b24d38ffa3824d8299
Victoriia <vburbura@gmail.com>
2024-11-15 17:44:51 +0200

    tests for isIsogram function

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,28 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true for the word "playgrounds"`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it(`should return false for the word "look"`, () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it(`should return false for the word "Adam" (case-insensitive)`, () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it(`should return false  for the word "Oops" (case-insensitive)`, () => {
+    expect(isIsogram('Oops')).toBe(false);
+  });
+
+  it(`should return false for the word "repetitions"`, () => {
+    expect(isIsogram('repetitions')).toBe(false);
+  });
+
+  it(`should return true for the word "isogram"`, () => {
+    expect(isIsogram('isogram')).toBe(true);
+  });
 });
