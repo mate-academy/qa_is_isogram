040134ac55ea81b05fe7c44e0c27260300ee3132
Diana Khalaim <khalaimdiana@gmail.com>
2024-11-15 14:45:58 +0200

    add tests for isIsogram.test.js

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,35 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return false for 2 repeating consecutive letters`, () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it(`should return false for 2 repeating non-consecutive letters`, () => {
+    expect(isIsogram('reduplicated')).toBe(false);
+  });
+
+  it(`should return false for 2 repeating`
+  + ` consecutive letters in different registers`, () => {
+    expect(isIsogram('Oops')).toBe(false);
+  });
+
+  it(`should return false for 2 repeating`
+  + ` non-consecutive letters in different registers`, () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it(`should return true for empty string`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`should return true for word with no repeating letters`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it(`should return true for word with no repeating letters`
+  + `in different registers`, () => {
+    expect(isIsogram('Playgrounds')).toBe(true);
+  });
 });
