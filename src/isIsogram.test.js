726859038aa4909897a7923360a38d84419f445f
Mizanin15 <tabachuk152@gmail.com>
2024-11-15 14:57:48 +0200

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,27 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should return true for a word with no repeating letters', () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it('should return false for a word with repeating letters', () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it('should return false for a case-sensitive repeating letters', () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it('should return true for empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(
+    'should return false for a word with case-insensitive repeating letters',
+    () => {
+      expect(isIsogram('Oops')).toBe(false);
+    }
+  );
 });
