998a474848e98b98f0f872ecccedd46e41adb8db
Sergio47ua <serhiivoitykappleid@gmail.com>
2024-11-22 16:41:31 +0100

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,16 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return "false" if 'word' has repeating letters`, () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it(`should return "true" if 'word' has no repeating letters`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it(`should return "true" if 'word' is empty`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
 });
