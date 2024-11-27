e86892868a6b986f93ed8310f53b678bf2857abb
KatOlista^^! <olya.katyushina@gmail.com>
2024-11-27 11:49:56 +0100

    add tests

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,28 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true if empty string`, () => {
+    const result = isIsogram('');
+
+    expect(result).toBe(true);
+  });
+
+  it(`should return true if isogram`, () => {
+    const result = isIsogram('playgrounds');
+
+    expect(result).toBeTruthy();
+  });
+
+  it(`should return false if is not isogram`, () => {
+    const result = isIsogram('look');
+
+    expect(result).toBeFalsy();
+  });
+
+  it(`should return false and be case-insensitive if is not isogram`, () => {
+    const result = isIsogram('Oops');
+
+    expect(result).toBeFalsy();
+  });
 });
