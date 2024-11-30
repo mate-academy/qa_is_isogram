ce19855be03eb6c17cc42dc55a2fcd5b3a82b761
Kseniia Krytska <kseniiakrytska@gmail.com>
2024-11-30 09:58:18 +0200

    tests are done

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,24 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('accepts isogram of both registers', () => {
+    expect(isIsogram('aqwertYUIOPDFgh')).toBeTruthy();
+  });
+
+  it('empty string is an isogram', () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
+
+  it('counts repeat of small letters ', () => {
+    expect(isIsogram('aqweartyuiolkjhg')).toBeFalsy();
+  });
+
+  it('counts repeat of capital letters ', () => {
+    expect(isIsogram('AQWERTYUIIOKJHG')).toBeFalsy();
+  });
+
+  it('counts repeat of different register letters', () => {
+    expect(isIsogram('wEtUaryu')).toBeFalsy();
+  });
 });
