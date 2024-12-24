ea4083d4ebc88c6d8edcedb299c19f45bd32f540
Dmytro Boiko <iamboikodmytro@gmail.com>
2024-12-24 11:11:11 +0200

    the task is complete

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -1,9 +1,25 @@
-'use strict';
+"use strict";
 
-describe('isIsogram', () => {
-  const { isIsogram } = require('./isIsogram');
+describe("isIsogram", () => {
+  const { isIsogram } = require("./isIsogram");
 
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
+  });
+
+  it(`should be 'true' if 'word' is 'playgrounds'`, () => {
+    expect(isIsogram("playgrounds")).toBe(true);
+  });
+
+  it(`should be 'false' if 'word' is 'look'`, () => {
+    expect(isIsogram("look")).toBe(false);
+  });
+
+  it(`should be 'false' if 'word' is 'Adam'`, () => {
+    expect(isIsogram("Adam")).toBe(false);
+  });
+
+  it(`should be 'true' if 'word' is empty string`, () => {
+    expect(isIsogram("")).toBe(true);
   });
 });
