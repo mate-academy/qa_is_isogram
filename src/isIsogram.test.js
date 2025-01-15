07694b97999e0fd8abe0a51bd9e305520b235289
Kostya Niekrasov <kostyanekrasov1501@gmail.com>
2025-01-15 13:51:52 +0200

    tests for isIsogram

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -1,9 +1,27 @@
-'use strict';
+"use strict";
 
-describe('isIsogram', () => {
-  const { isIsogram } = require('./isIsogram');
+describe("isIsogram", () => {
+  const { isIsogram } = require("./isIsogram");
 
-  it(`should be declared`, () => {
+  it("should be declared", () => {
     expect(isIsogram).toBeInstanceOf(Function);
+  });
+
+  it("should ignore register", () => {
+    const res = isIsogram("lOok");
+
+    expect(res).toBe(false);
+  });
+
+  it("the empty string is an isogram", () => {
+    const res = isIsogram("");
+
+    expect(res).toBe(true);
+  });
+
+  it("should return true if the characters are unique", () => {
+    const res = isIsogram("playgrounds");
+
+    expect(res).toBe(true);
   });
 });
