f8a85b2abb51399ce42c4721a1e0bbc91561a361
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2025-01-21 13:15:38 +0200

    solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,32 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should consider empty string as isogram`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`should be case-insensitive`, () => {
+    expect(isIsogram('Oops')).toBe(false);
+  });
+
+  it(`should return true for 'playgrounds'`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it(`should return fasle for 'look'`, () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it(`should return true for single char`, () => {
+    expect(isIsogram('a')).toBe(true);
+  });
+
+  it(`should return true for isogram with special chars`, () => {
+    expect(isIsogram('123*#asdf')).toBe(true);
+  });
+
+  it(`should return false for not unique input with special chars`, () => {
+    expect(isIsogram('123-*-#asdf')).toBe(false);
+  });
 });
