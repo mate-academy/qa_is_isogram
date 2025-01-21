9fa287234d0c886110f42f452e4a7948b7ea88f1
jdifek <givotkov11@gmail.com>
2025-01-21 13:35:02 +0200

    fdfdsfd

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -1,9 +1,41 @@
 'use strict';
 
-describe('isIsogram', () => {
-  const { isIsogram } = require('./isIsogram');
+const { isIsogram } = require('./isIsogram');
 
-  it(`should be declared`, () => {
+describe('isIsogram', () => {
+  it('should be declared', () => {
     expect(isIsogram).toBeInstanceOf(Function);
+  });
+
+  test('should return true for "playgrounds"', () => {
+    expect(isIsogram('playgrounds')).toEqual(true);
+  });
+
+  test('should return false for "look"', () => {
+    expect(isIsogram('look')).toEqual(false);
+  });
+
+  test('should return false for "Adam"', () => {
+    expect(isIsogram('Adam')).toEqual(false);
+  });
+
+  test('should return true for an empty string', () => {
+    expect(isIsogram('')).toEqual(true);
+  });
+
+  test('should return false for "Oops"', () => {
+    expect(isIsogram('Oops')).toEqual(false);
+  });
+
+  test('should return true for "abc"', () => {
+    expect(isIsogram('abc')).toEqual(true);
+  });
+
+  test('should return true for "AAB"', () => {
+    expect(isIsogram('AAB')).toEqual(false);
+  });
+
+  test('should return true for "Aba"', () => {
+    expect(isIsogram('Aba')).toEqual(false);
   });
 });
