c0d6a611d1440c252e46c6d2afd259b61b682108
Svitlana <svitlana.gaiduk@gmail.com>
2025-01-28 18:18:02 +0200

    add task solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true for an empty string`, () => {
+    expect(isIsogram('')).toEqual(true);
+  });
+
+  it(`should return true for a word with no repeating letters`, () => {
+    expect(isIsogram('playgrounds')).toEqual(true);
+  });
+
+  it(`should return false for a word with repeating letters`, () => {
+    expect(isIsogram('look')).toEqual(false);
+  });
+
+  it(`should be case-insensitive with repeating letters`, () => {
+    expect(isIsogram('Oops')).toEqual(false);
+  });
 });
