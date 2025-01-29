1ebfd582281ef6b3861c2892a7efaa11182e7955
Nataliia Henda <nataliehenda1296@gmail.com>
2025-01-29 12:13:24 +0200

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`return true for empty string`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`return true if chars doesn't repeat`, () => {
+    expect(isIsogram('abc')).toBe(true);
+  });
+
+  it(`return false if chars repeat`, () => {
+    expect(isIsogram('abcb')).toBe(false);
+  });
+
+  it(`is case-insensitive, uppercase and lowercase is the same symbol`, () => {
+    expect(isIsogram('abcC')).toBe(false);
+  });
 });
