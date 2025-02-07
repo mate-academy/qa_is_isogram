3c70d905dd9a7596171b87754434cf7218d750e3
Dmytro Ivasenko <dmtivasenko@gmail.com>
2025-02-07 17:16:25 +0400

    add solution

--- src/isIsogram.js
+++ src/isIsogram.js
@@ -8,15 +8,9 @@
 function isIsogram(word) {
   const wordToLower = word.toLowerCase();
 
-  for (let i = 0; i < wordToLower.length; i++) {
-    const letter = wordToLower[i];
+  const set = new Set(wordToLower.split(''));
 
-    if (wordToLower.includes(letter, i + 1)) {
-      return false;
-    }
-  }
-
-  return true;
+  return set.size === wordToLower.length;
 }
 
 module.exports = { isIsogram };
