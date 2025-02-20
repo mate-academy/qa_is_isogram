511324ee32f63b1388dddcb075c511282b20f753
Nastya <anastasiia.y.sitnikova@gmail.com>
2025-02-20 12:16:58 +0100

    add solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true for empty string`, () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
+
+  it(`should be case-insensitive`, () => {
+    expect(isIsogram('Adam')).toBeFalsy();
+  });
+
+  it(`should return true if the word has no repeating letters`, () => {
+    expect(isIsogram('playgrounds')).toBeTruthy();
+  });
+
+  it(`should return false for words with repeating letters`, () => {
+    expect(isIsogram('hello')).toBeFalsy();
+  });
 });
