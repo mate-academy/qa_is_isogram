f2a8f4b25e021ab0ffcb239582d7b870bf07044d
Vlad Zahorovskii <zagorovskii12@gmail.com>
2025-02-21 21:06:40 +0200

    Done

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,22 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should return boolean', () => {
+    expect(typeof isIsogram('')).toBe('boolean');
+  });
+
+  it('should return true for word with unique letters', () => {
+    expect(isIsogram('playground')).toBe(true);
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it('should return false for same letters', () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it('should be case-insensitive', () => {
+    expect(isIsogram('Adam')).toBe(false);
+    expect(isIsogram('Oops')).toBe(false);
+  });
 });
