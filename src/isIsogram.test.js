3c74890bf73e45424de44a7dbddcb86cb6ca3c55
Ruslan Hrynchuk <r.momos@gmail.com>
2025-03-05 12:13:26 +0200

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -3,7 +3,43 @@
 describe('isIsogram', () => {
   const { isIsogram } = require('./isIsogram');
 
-  it(`should be declared`, () => {
+  it('should be declared', () => {
     expect(isIsogram).toBeInstanceOf(Function);
+  });
+
+  it('should return true with an empty string', () => {
+    const result = isIsogram('');
+
+    expect(result).toBeTruthy();
+  });
+
+  it('should return true with a single letter', () => {
+    const result = isIsogram('c');
+
+    expect(result).toBeTruthy();
+  });
+
+  it('should return true for a word with special characters', () => {
+    const result = isIsogram('a-b_c');
+
+    expect(result).toBeTruthy();
+  });
+
+  it('should return true for a word with numbers', () => {
+    const result = isIsogram('ab2c3');
+
+    expect(result).toBeTruthy();
+  });
+
+  it('should return true for an isogram word', () => {
+    const result = isIsogram('playgrounds');
+
+    expect(result).toBeTruthy();
+  });
+
+  it('should return false for a word with repeating in different cases', () => {
+    const result = isIsogram('Adam');
+
+    expect(result).toBeFalsy();
   });
 });
