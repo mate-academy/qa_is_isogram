1a6550791d833e832997ad99c604952498c992c3
Andrii <andriykostiuk1@gmail.com>
2025-03-06 09:51:35 +0200

    add solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,40 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('empty string', () => {
+    const res = isIsogram('');
+
+    expect(res).toBe(true);
+  });
+
+  it('should be case-insensitive', () => {
+    const res = isIsogram('Adam');
+
+    expect(res).toBe(false);
+  });
+
+  it('should return correct value', () => {
+    const res = isIsogram('playgrounds');
+
+    expect(res).toBe(true);
+  });
+
+  it('single character', () => {
+    const res = isIsogram('b');
+
+    expect(res).toBe(true);
+  });
+
+  it('words with repeating letters', () => {
+    const res = isIsogram('bab');
+
+    expect(res).toBe(false);
+  });
+
+  it('should handle words with hyphens or spaces correctly', () => {
+    const res = isIsogram('sub-dermatoglyphic');
+
+    expect(res).toBe(true);
+  });
 });
