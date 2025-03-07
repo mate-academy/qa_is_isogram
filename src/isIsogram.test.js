c83118b5f364d5143c587305c05fedd7200228a0
Anastasiia <olina.anastasiya@gmail.com>
2025-03-07 17:14:33 +0200

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,28 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should return a string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it('should contain only letters', () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it('should check that there are no repeating letters', () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it('should return true if there is empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it('should be case-insensetive', () => {
+    expect(isIsogram('PlayGrounds')).toBe(true);
+  });
+
+  it('should be consecutive or non-consecutive', () => {
+    expect(isIsogram('paly')).toBe(true);
+  });
 });
