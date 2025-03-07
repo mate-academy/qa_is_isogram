5044df6aedd634c949041b785e5c0fbd35281d28
Dmytro Bulavka <dgbulavka@gmail.com>
2025-03-07 17:31:34 +0200

    tests for isIsogram

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,24 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('Should retern "true" if argument is "playgrounds"', () => {
+    expect(isIsogram('playgrounds')).toBeTruthy();
+  });
+
+  it('Should retern "false" if argument is "look"', () => {
+    expect(isIsogram('look')).toBeFalsy();
+  });
+
+  it('Should retern "false" if argument is "Adam"', () => {
+    expect(isIsogram('look')).toBeFalsy();
+  });
+
+  it('Should retern "true" if argument is empty string', () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
+
+  it('Should retern "false" if argument is "Oops"', () => {
+    expect(isIsogram('Oops')).toBeFalsy();
+  });
 });
