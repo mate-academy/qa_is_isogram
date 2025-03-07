1ff4b1cbfd24e208cac0a2317cd4ff29ff858747
Tetiana Kravchenko <landuwof@gmail.com>
2025-03-07 15:31:17 +0200

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,25 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true for 'playgrounds'`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it(`should return false for 'look'`, () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it(`should return true for ''`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`should return false for 'Adam'`, () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it(`should return false for 'Oops'`, () => {
+    expect(isIsogram('Oops')).toBe(false);
+  });
+
 });
