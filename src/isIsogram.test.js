bd6183cdf128242f5f5a997c107d2ee6017a3c31
RuslanKravchuk2907 <meladzeruslan164@gmail.com>
2025-03-08 16:24:33 +0200

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should return false for repeated letters', () => {
+    expect(isIsogram('hello')).toBe(false);
+  });
+
+  it('should return true for empty field', () => {
+    expect(isIsogram(' ')).toBe(true);
+  });
+
+  it('should return true for not repited letters', () => {
+    expect(isIsogram('Ruslan')).toBe(true);
+  });
+
+  it('should return false if a word contains a repeating upper and lower case letter', () => {
+    expect(isIsogram('Mama')).toBe(false);
+  });
 });
