089dfe20204c1594c10f47785aa296537096de73
VladBlonskyi <vladblonskyi@gmail.com>
2025-03-09 19:18:00 +0200

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -1,9 +1,29 @@
-'use strict';
+"use strict";
 
-describe('isIsogram', () => {
-  const { isIsogram } = require('./isIsogram');
+describe("isIsogram", () => {
+  const { isIsogram } = require("./isIsogram");
 
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
+  });
+
+  it(`playgrounds should return TRUE`, () => {
+    expect(isIsogram("playgrounds")).toBe(true);
+  });
+
+  it(`look should return False`, () => {
+    expect(isIsogram("look")).toBe(false);
+  });
+
+  it(`Adam should return False`, () => {
+    expect(isIsogram("Adam")).toBe(false);
+  });
+
+  it(`Empty should return True`, () => {
+    expect(isIsogram("")).toBe(true);
+  });
+
+  it(`Oops should return False`, () => {
+    expect(isIsogram("Oops")).toBe(false);
   });
 });
