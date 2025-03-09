7e66e6252742e11a6528b4ea72bb842e648ce651
davidrotar <rotardavid378@gmail.com>
2025-03-09 23:04:56 +0200

    Add tests

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,24 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`return 'true' for 'playgrounds'`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it(`return 'false' for 'look'`, () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it(`return 'false' for 'Adam'`, () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it(`return 'true' for empty string`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`return 'false' for 'Oops'`, () => {
+    expect(isIsogram('OOps')).toBe(false);
+  });
 });
