d99b828dd2f7d76a234a6682d06d4d1e61a3c7bd
Maria <m.gosp1704@gmail.com>
2025-03-10 15:43:34 +0200

    solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,24 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should return only boolean values', () => {
+    expect(typeof isIsogram('cat')).toBe('boolean');
+  });
+
+  it('should return true if empty array', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it('should be case-insensitive', () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it(`should return true if 'str' is isogram`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it(`should return false if 'str' is not isogram`, () => {
+    expect(isIsogram('look')).toBe(false);
+  });
 });
