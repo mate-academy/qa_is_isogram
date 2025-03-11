e92405c6473fb3dafaa318a93489983bf25ee9f2
Dana Sapozhnikova <sapozhnikovabogdana@gmail.com>
2025-03-11 16:06:04 +0200

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,24 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  test('is true if isogram shows name', () => {
+    expect(isIsogram('Adam')).toBeFalsy();
+  });
+
+  test('is empty line', () => {
+    expect(isIsogram(' ')).toBeTruthy();
+  });
+
+  test('is true for non-repeating letters', () => {
+    expect(isIsogram('playgrounds')).toBeTruthy();
+  });
+
+  test('is false for repeating letters', () => {
+    expect(isIsogram('look')).toBeFalsy();
+  });
+
+  test('is false for repeating case-insensitive letters', () => {
+    expect(isIsogram('Adam')).toBeFalsy();
+  });
 });
