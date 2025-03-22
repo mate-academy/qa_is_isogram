9c555bda81ff54d29eb414b70f672a296f687fde
Alona Zherdetska <alionazhv@gmail.com>
2025-03-22 20:04:39 +0100

    test fix

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -3,27 +3,40 @@
 describe('isIsogram', () => {
   const { isIsogram } = require('./isIsogram');
 
-  it(`should be declared`, () => {
-    expect(isIsogram).toBeInstanceOf(Function);
+  it('should be declared', () => {
+    expect(isIsogram)
+      .toBeInstanceOf(Function);
   });
 
-  it(`should return true for the word which is 
-    isIogram`, () => {
-    expect(isIsogram('word')).toEqual(true);
-  });
+  it(
+    'should return true for the word which is isIogram',
+    () => {
+      expect(isIsogram('word'))
+        .toEqual(true);
+    }
+  );
 
-  it(`should return false for the word with repeated 
-    letters`, () => {
-    expect(isIsogram('posibble')).toEqual(false);
-  })
+  it(
+    'should return false for the word with repeated letters',
+    () => {
+      expect(isIsogram('posibble'))
+        .toEqual(false);
+    }
+  );
 
-  it(`should return true for the long word, 
-    e.g.Transitions`, () => {
-    expect(isIsogram('Transitions')).toEqual(true);
-  })
+  it(
+    'should return true for the long word, e.g. Transitions',
+    () => {
+      expect(isIsogram('Transitions'))
+        .toEqual(true);
+    }
+  );
 
-  it(`should return true for the empty 
-    string`, () => {
-    expect(isIsogram('').toEqual(true));
-  })
+  it(
+    'should return true for the empty string',
+    () => {
+      expect(isIsogram(''))
+        .toEqual(true);
+    }
+  );
 });
