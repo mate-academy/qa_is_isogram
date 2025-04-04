8b855b11e5c6a80dc53682c17586f6527540f802
JonSON124 <jonsonmark532@proton.me>
2025-04-04 13:18:52 +0300

    complete

--- src/isIsogram.js
+++ src/isIsogram.js
@@ -8,6 +8,10 @@
 function isIsogram(word) {
   const wordToLower = word.toLowerCase();
 
+  if (/\W/.test(wordToLower)) {
+    throw new Error('Invalid word');
+  }
+
   for (let i = 0; i < wordToLower.length; i++) {
     const letter = wordToLower[i];
 
--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,25 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`true for an empty string`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`true for unique letters`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it(`false for repeated letters`, () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it(`case sensitive`, () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it('should be a word', () => {
+    expect(() => isIsogram('  abc,  def-gh-ijk!!??  '))
+      .toThrow(new Error('Invalid word'));
+  });
 });
