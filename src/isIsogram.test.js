b9378c47da487cc4084af365014b364bbf468fa3
Janani <jananiraja1405@gmail.com>
2025-04-23 13:51:47 +1200

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,21 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should return true if string is empty', () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
+
+  it('should return true if string does not have duplicate letters', () => {
+    expect(isIsogram('Dino')).toBeTruthy();
+  });
+
+  it('should return false if string has duplicate letters', () => {
+    expect(isIsogram('penguin')).toBeFalsy();
+  });
+
+  it('should return false even if duplicate letters are in different case',
+    () => {
+      expect(isIsogram('peacE')).toBeFalsy();
+    });
 });
