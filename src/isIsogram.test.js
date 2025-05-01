77c97055b96f09c41a4ed4be875f02d87e38bc86
nadia vlasenko <nadiia@MacBook-Air-nadia.local>
2025-05-01 20:23:50 +0200

    sol

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,14 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should return true for an empty string', () => {
+    expect(isIsogram('')).toEqual(true);
+  });
+
+  it('should return true for a string with digits in any case', () => {
+    expect(isIsogram('playgrounds')).toEqual(true);
+    expect(isIsogram('look')).toEqual(false);
+    expect(isIsogram('Oops')).toEqual(false);
+  });
 });
