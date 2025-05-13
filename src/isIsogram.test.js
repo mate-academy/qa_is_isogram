5abd0ec06d78046c23c4150df4648064daa22ec8
Maryna <marina.avramenko123@gmail.com>
2025-05-13 18:48:44 +0300

    add tests

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -1,9 +1,29 @@
-'use strict';
+"use strict";
 
-describe('isIsogram', () => {
-  const { isIsogram } = require('./isIsogram');
+describe("isIsogram", () => {
+  const { isIsogram } = require("./isIsogram");
 
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
+  });
+
+  it(`should return true for 'playgrounds'`, () => {
+    expect(isIsogram("playgrounds")).toBe(true);
+  });
+
+  it(`should return false for 'look'`, () => {
+    expect(isIsogram("look")).toBe(false);
+  });
+
+  it(`should return false for 'Adam'`, () => {
+    expect(isIsogram("Adam")).toBe(false);
+  });
+
+  it("should be return  true for empty string", () => {
+    expect(isIsogram("")).toBe(true);
+  });
+
+  it("should be return  false for (Oops) ", () => {
+    expect(isIsogram("Oops")).toBe(false);
   });
 });
