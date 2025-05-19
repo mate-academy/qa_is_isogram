843bb55a3c5ea2f4e9474d3661388d039a5b082f
Roman Romanchuk <roma.roman4uk.48@gmail.com>
2025-05-19 21:39:43 +0200

    solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,34 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return fasel for 'hello '`, () => {
+    const result = isIsogram('hello');
+
+    expect(result).toBe(false);
+  });
+
+  it(`should return fasel for 'moOse'`, () => {
+    const result = isIsogram('moOse');
+
+    expect(result).toBe(false);
+  });
+
+  it(`should return true for empty string`, () => {
+    const result = isIsogram('');
+
+    expect(result).toBe(true);
+  });
+
+  it(`should return true for 'background'`, () => {
+    const result = isIsogram('background');
+
+    expect(result).toBe(true);
+  });
+
+  it(`should return true for 'subdermatoglyphic'`, () => {
+    const result = isIsogram('subdermatoglyphic');
+
+    expect(result).toBe(true);
+  });
 });
