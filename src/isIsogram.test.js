7707907b1f0f8e8bbfbcf8858365fd30b3139df3
anIRI26 <shtefanesa.iri@gmail.com>
2025-05-27 20:05:34 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,24 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should be isogam`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`should be unique word`, () => {
+    expect(isIsogram('apple')).toBe(false);
+  });
+
+  it(`should be isogam`, () => {
+    expect(isIsogram('Aplle')).toBe(false);
+  });
+
+  it(`should be isogam`, () => {
+    expect(isIsogram('GREY')).toBe(true);
+  });
+
+  it(`should be isogam`, () => {
+    expect(isIsogram('grey')).toBe(true);
+  });
 });
