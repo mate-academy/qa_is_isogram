c76445a881105024a6408f563f21df4866f985f0
Stas <stas.mylashenko@gmail.com>
2025-05-27 17:04:04 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  test(`should return true for 'playgrounds'`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  test(`should return false for 'look'`, () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  test(`should return false for 'Adam'`, () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  test(`should return true for an empty string`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
 });
