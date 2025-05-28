6a6ebab0f233223e12300ab7c021c687e3ee7212
MykolaDekalchuk <dekinikolaj27@gmail.com>
2025-05-28 15:36:54 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,24 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true for long valid isogram`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it(`should return true for valid isogram with upper case`, () => {
+    expect(isIsogram('MonEta')).toBe(true);
+  });
+
+  it(`should return false for invalid isogram`, () => {
+    expect(isIsogram('moon')).toBe(false);
+  });
+
+  it(`should return false for invalid isogram with upper case`, () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it(`should return true for an empty message`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
 });
