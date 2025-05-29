fc0c417067cc751614f8f4996a4e531faf448078
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2025-05-29 17:03:06 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,35 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true for empty string (including one space) `, () => {
+    expect(isIsogram('')).toBe(true);
+    expect(isIsogram(' ')).toBe(true);
+  });
+
+  it('should return true for single character string', () => {
+    expect(isIsogram('a')).toBe(true);
+    expect(isIsogram('Z')).toBe(true);
+  });
+
+  it('should return true for isogram word (all unique letters)', () => {
+    expect(isIsogram('ioGram')).toBe(true);
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it('should return false for multiple spaces', () => {
+    expect(isIsogram('  ')).toBe(false);
+    expect(isIsogram('a  a')).toBe(false);
+    expect(isIsogram('isog ram ')).toBe(false);
+    expect(isIsogram('a b c')).toBe(false);
+  });
+
+  it(
+    'should return false if word has duplicate letters'
+      + ' (case insensitive)',
+    () => {
+      expect(isIsogram('Adam')).toBe(false);
+      expect(isIsogram('aba')).toBe(false);
+    }
+  );
 });
