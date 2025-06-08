9e827a271c8b8e5c6ca58dd73f0dbc1d4a1e0ebf
Sasha <sashatsapko91@gmail.com>
2025-06-08 16:28:45 +0300

    solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -1,9 +1,28 @@
-'use strict';
-
+// eslint-disable-next-line strict
 describe('isIsogram', () => {
   const { isIsogram } = require('./isIsogram');
 
-  it(`should be declared`, () => {
+  it('should be declared', () => {
     expect(isIsogram).toBeInstanceOf(Function);
+  });
+
+  it('should return true for "playgrounds"', () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it('should return false for "look"', () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it('should return false for "Adam" (case-insensitive)', () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it('should return true for empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it('should return false for "Oops"', () => {
+    expect(isIsogram('Oops')).toBe(false);
   });
 });
