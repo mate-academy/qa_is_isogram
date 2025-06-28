3ce8e3fef4274b2357fb77030f6c7918de3ceb69
Hryniuk_Taras <gritaras484@gmail.com>
2025-06-28 13:15:45 +0300

    solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true for isogram`, () => {
+    expect(isIsogram('isogram')).toBe(true);
+  });
+
+  it('should be case-insensitive', () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it('should return true for empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it('should return false for non-isogram', () => {
+    expect(isIsogram('eleven')).toBe(false);
+  });
 });
