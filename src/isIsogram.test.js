5e684c6e2d0c93c55ef4ae8442e791e44d515ee4
Iryna Zahorodnia <irishas25@ukr.net>
2025-07-05 09:00:13 +0300

    test is isogram

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,28 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return boolean`, () => {
+    expect(typeof isIsogram('')).toBe('boolean');
+  });
+
+  it(`should return true for ''`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`should return true for 'playgrounds'`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it(`should return false for 'look'`, () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it(`should return false for 'Adam'`, () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it(`should be case-insensitive`, () => {
+    expect(isIsogram('Oops')).toBe(false);
+  });
 });
