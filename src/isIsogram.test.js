f88a794d78b072aab86449a27051f9415cb4c88b
Marianna Matola <mutnenka.mk@gmail.com>
2025-07-05 21:21:17 +0300

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,42 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should be true for isogram`, () => {
+    const word = 'playgrounds';
+
+    const result = isIsogram(word);
+
+    expect(result).toBe(true);
+  });
+
+
+  it(`should be false for double letters`, () => {
+    const word = 'look';
+
+    const result = isIsogram(word);
+
+    expect(result).toBe(false);
+  });
+
+
+  it(`should be false for two same letters`, () => {
+    const word = 'Adam';
+
+    const result = isIsogram(word);
+
+    expect(result).toBe(false);
+  });
+
+
+  it(`should be true for empty string`, () => {
+    const word = '';
+
+    const result = isIsogram(word);
+
+    expect(result).toBe(true);
+  });
+
+
+
 });
